2a0bc33fa2de4897d6e86d5323381e23cf824572
Shige Takeda <smtakeda@gmail.com>
2017-01-12 20:38:57 +0000

    SNOW-26789: Test: Fixed OverflowError caused by invalid range of timetamp data for SnowSQL.

---
+++ test/test_snowsql_timestamp_format.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2012-2017 Snowflake Computing Inc. All right reserved.
+#
+import snowflake.connector
+
+
+def test_snowsql_timestamp_format(db_parameters):
+    """
+    In SnowSQL, OverflowError should not happen
+    """
+    connection = snowflake.connector.connect(
+        protocol=db_parameters['protocol'],
+        account=db_parameters['account'],
+        user=db_parameters['user'],
+        password=db_parameters['password'],
+        host=db_parameters['host'],
+        port=db_parameters['port'],
+        database=db_parameters['database'],
+        schema=db_parameters['schema'],
+        is_sfsql=True,
+    )
+    ret = connection.cursor().execute(
+        "SELECT '19999-09-30 12:34:56'::timestamp_ltz").fetchone()
+    assert ret[0] == 'Thu, 30 Sep 19999 19:34:56 +0000'
+
+    ret = connection.cursor().execute(
+        "SELECT '19999-09-30 12:34:56'::timestamp_ntz").fetchone()
+    assert ret[0] == 'Thu, 30 Sep 19999 12:34:56 +0000'
+
+    # NOTE timestamp_tz doesn't accept the timestamp out of range
+    # what is the range?
