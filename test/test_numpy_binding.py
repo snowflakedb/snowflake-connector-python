7d0e305ff387acdb8ff21fed044e6d3de934175a
Shige Takeda <smtakeda@gmail.com>
2017-03-23 21:48:09 +0000

    SNOW-28608: fixed various timestamp converter issues

--- test/test_numpy_binding.py
+++ test/test_numpy_binding.py
@@ -7,10 +7,32 @@ def test_numpy_datatype_binding(conn_cnx, db_parameters):
     """
     Tests numpy data type binding
     """
-    MAX_FLOAT = '1.79769313486e+308'
     epoch_time = int(time.time()) * 1000000000 + 123456789
-    current_time = np.datetime64(epoch_time, 'ns')
-    specific_date = np.datetime64('2005-02-25T03:30Z')
+    all_data = [{
+        'tz': 'America/Los_Angeles',
+        'float': '1.79769313486e+308',
+        'epoch_time': epoch_time,
+        'current_time': np.datetime64(epoch_time, 'ns'),
+        'specific_date': np.datetime64('2005-02-25T03:30Z')
+    }, {
+        'tz': 'Asia/Tokyo',
+        'float': '-1.79769313486e+308',
+        'epoch_time': epoch_time,
+        'current_time': np.datetime64(epoch_time, 'ns'),
+        'specific_date': np.datetime64('1970-12-31T05:00:00Z')
+    }, {
+        'tz': 'America/New_York',
+        'float': '-1.79769313486e+308',
+        'epoch_time': epoch_time,
+        'current_time': np.datetime64(epoch_time, 'ns'),
+        'specific_date': np.datetime64('1969-12-31T05:00:00Z')
+    }, {
+        'tz': 'UTC',
+        'float': '-1.79769313486e+308',
+        'epoch_time': epoch_time,
+        'current_time': np.datetime64(epoch_time, 'ns'),
+        'specific_date': np.datetime64('1968-11-12T07:00:00.123Z')
+    }]
     try:
         with conn_cnx(use_numpy=True) as cnx:
             cnx.cursor().execute("""
@@ -27,7 +49,10 @@ CREATE OR REPLACE TABLE {name} (
     c10 timestamp_ltz, -- datetime64,
     c11 timestamp_tz)  -- datetime64
             """.format(name=db_parameters['name']))
-            cnx.cursor().execute("""
+            for data in all_data:
+                cnx.cursor().execute("""
+ALTER SESSION SET timezone='{tz}'""".format(tz=data['tz']))
+                cnx.cursor().execute("""
 INSERT INTO {name}(
     c1,
     c2,
@@ -53,20 +78,20 @@ VALUES(
     %s,
     %s,
     %s)""".format(
-                name=db_parameters['name']), (
-                np.iinfo(np.int8).max,
-                np.iinfo(np.int16).max,
-                np.iinfo(np.int32).max,
-                np.iinfo(np.int64).max,
-                np.finfo(np.float16).max,
-                np.finfo(np.float32).max,
-                np.float64(MAX_FLOAT),
-                current_time,
-                current_time,
-                current_time,
-                specific_date,
-            ))
-            rec = cnx.cursor().execute("""
+                    name=db_parameters['name']), (
+                    np.iinfo(np.int8).max,
+                    np.iinfo(np.int16).max,
+                    np.iinfo(np.int32).max,
+                    np.iinfo(np.int64).max,
+                    np.finfo(np.float16).max,
+                    np.finfo(np.float32).max,
+                    np.float64(data['float']),
+                    data['current_time'],
+                    data['current_time'],
+                    data['current_time'],
+                    data['specific_date'],
+                ))
+                rec = cnx.cursor().execute("""
 SELECT
        c1,
        c2,
@@ -80,18 +105,20 @@ SELECT
        c10,
        c11
   FROM {name}""".format(
-                name=db_parameters['name'])).fetchone()
-            assert np.int8(rec[0]) == np.iinfo(np.int8).max
-            assert np.int16(rec[1]) == np.iinfo(np.int16).max
-            assert np.int32(rec[2]) == np.iinfo(np.int32).max
-            assert np.int64(rec[3]) == np.iinfo(np.int64).max
-            assert np.float16(rec[4]) == np.finfo(np.float16).max
-            assert np.float32(rec[5]) == np.finfo(np.float32).max
-            assert rec[6] == np.float64(MAX_FLOAT)
-            assert rec[7] == current_time
-            assert str(rec[8]) == str(current_time)[0:10]
-            assert rec[9] == current_time
-            assert rec[10] == specific_date
+                    name=db_parameters['name'])).fetchone()
+                assert np.int8(rec[0]) == np.iinfo(np.int8).max
+                assert np.int16(rec[1]) == np.iinfo(np.int16).max
+                assert np.int32(rec[2]) == np.iinfo(np.int32).max
+                assert np.int64(rec[3]) == np.iinfo(np.int64).max
+                assert np.float16(rec[4]) == np.finfo(np.float16).max
+                assert np.float32(rec[5]) == np.finfo(np.float32).max
+                assert rec[6] == np.float64(data['float'])
+                assert rec[7] == data['current_time']
+                assert str(rec[8]) == str(data['current_time'])[0:10]
+                assert rec[9] == data['current_time']
+                assert rec[10] == data['specific_date']
+                cnx.cursor().execute("""
+DELETE FROM {name}""".format(name=db_parameters['name']))
     finally:
         with conn_cnx() as cnx:
             cnx.cursor().execute("""
