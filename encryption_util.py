4cf4d38f0e6488d76e88de208810c2c78d92e34f
Shige Takeda <smtakeda@gmail.com>
2017-10-04 21:43:10 +0000

    SNOW-31980: Refactor python client for azure work

---
+++ encryption_util.py
@@ -0,0 +1,164 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2012-2017 Snowflake Computing Inc. All right reserved.
+#
+
+
+import base64
+
+from logging import getLogger
+import json
+from Crypto.Cipher import AES
+import os
+import tempfile
+from collections import namedtuple
+from .compat import (PKCS5_PAD, PKCS5_UNPAD, PKCS5_OFFSET, TO_UNICODE)
+from .constants import UTF8
+
+def matdesc_to_unicode(matdesc):
+    """
+    Convert Material Descriptor to Unicode String
+    """
+    return TO_UNICODE(
+        json.dumps({
+            u'queryId': matdesc.query_id,
+            u'smkId': str(matdesc.smk_id),
+            u'keySize': str(matdesc.key_size)
+        },
+            separators=(',', ':')))
+
+
+"""
+Material Description
+"""
+MaterialDescriptor = namedtuple(
+    "MaterialDescriptor", [
+        "smk_id",  # SMK id
+        "query_id",  # query id
+        "key_size"  # key size, 128 or 256
+    ]
+)
+
+"""
+Metadata for encrpytion
+"""
+EncryptionMetadata = namedtuple(
+    "EncryptionMetadata", [
+        "key",
+        "iv",
+        "matdesc"
+    ]
+)
+
+class SnowflakeEncryptionUtil(object):
+    @staticmethod
+    def get_secure_random(byte_length):
+        return os.urandom(byte_length)
+
+
+    @staticmethod
+    def encrypt_file(encryption_material, in_filename,
+                     chunk_size=AES.block_size * 4 * 1024, tmp_dir=None):
+        """
+        Encrypts a file
+        :param s3_metadata: S3 metadata output
+        :param encryption_material: encryption material
+        :param in_filename: input file name
+        :param chunk_size: read chunk size
+        :param tmp_dir: temporary directory, optional
+        :return: a encrypted file
+        """
+        logger = getLogger(__name__)
+        decoded_key = base64.standard_b64decode(
+            encryption_material.query_stage_master_key)
+        key_size = len(decoded_key)
+        logger.debug(u'key_size = %s', key_size)
+
+        # Generate key for data encryption
+        iv_data = SnowflakeEncryptionUtil.get_secure_random(AES.block_size)
+        file_key = SnowflakeEncryptionUtil.get_secure_random(key_size)
+        data_cipher = AES.new(key=file_key, mode=AES.MODE_CBC, IV=iv_data)
+
+        temp_output_fd, temp_output_file = tempfile.mkstemp(
+            text=False, dir=tmp_dir,
+            prefix=os.path.basename(in_filename) + "#")
+        padded = False
+        logger.debug(u'unencrypted file: %s, temp file: %s, tmp_dir: %s',
+                     in_filename, temp_output_file, tmp_dir)
+        with open(in_filename, u'rb') as infile:
+            with os.fdopen(temp_output_fd, u'wb') as outfile:
+                while True:
+                    chunk = infile.read(chunk_size)
+                    if len(chunk) == 0:
+                        break
+                    elif len(chunk) % AES.block_size != 0:
+                        chunk = PKCS5_PAD(chunk, AES.block_size)
+                        padded = True
+                    outfile.write(data_cipher.encrypt(chunk))
+                if not padded:
+                    outfile.write(data_cipher.encrypt(
+                        AES.block_size * chr(AES.block_size).encode(UTF8)))
+
+        # encrypt key with QRMK
+        key_cipher = AES.new(key=decoded_key, mode=AES.MODE_ECB)
+        enc_kek = key_cipher.encrypt(PKCS5_PAD(file_key, AES.block_size))
+
+        mat_desc = MaterialDescriptor(
+            smk_id=encryption_material.smk_id,
+            query_id=encryption_material.query_id,
+            key_size=key_size * 8)
+        metadata = EncryptionMetadata(
+            key=base64.b64encode(enc_kek).decode('utf-8'),
+            iv=base64.b64encode(iv_data).decode('utf-8'),
+            matdesc=matdesc_to_unicode(mat_desc),
+        )
+        return (metadata, temp_output_file)
+
+
+    @staticmethod
+    def decrypt_file(metadata, encryption_material, in_filename,
+                     chunk_size=AES.block_size * 4 * 1024, tmp_dir=None):
+        """
+        Decrypts a file and stores the output in the temporary directory
+        :param metadata: metadata input
+        :param encryption_material: encryption material
+        :param in_filename: input file name
+        :param chunk_size: read chunk size
+        :param tmp_dir: temporary directory, optional
+        :return: a decrypted file name
+        """
+        logger = getLogger(__name__)
+        key_base64 = metadata.key
+        iv_base64 = metadata.iv
+        decoded_key = base64.standard_b64decode(
+            encryption_material.query_stage_master_key)
+        key_bytes = base64.standard_b64decode(key_base64)
+        iv_bytes = base64.standard_b64decode(iv_base64)
+
+        key_cipher = AES.new(key=decoded_key, mode=AES.MODE_ECB)
+        file_key = PKCS5_UNPAD(key_cipher.decrypt(key_bytes))
+
+        data_cipher = AES.new(key=file_key, mode=AES.MODE_CBC, IV=iv_bytes)
+
+        temp_output_fd, temp_output_file = tempfile.mkstemp(
+            text=False, dir=tmp_dir,
+            prefix=os.path.basename(in_filename) + "#")
+        total_file_size = 0
+        prev_chunk = None
+        logger.info(u'encrypted file: %s, tmp file: %s',
+                    in_filename, temp_output_file)
+        with open(in_filename, u'rb') as infile:
+            with os.fdopen(temp_output_fd, u'wb') as outfile:
+                while True:
+                    chunk = infile.read(chunk_size)
+                    if len(chunk) == 0:
+                        break
+                    total_file_size += len(chunk)
+                    d = data_cipher.decrypt(chunk)
+                    outfile.write(d)
+                    prev_chunk = d
+                if prev_chunk is not None:
+                    total_file_size -= PKCS5_OFFSET(prev_chunk)
+                outfile.truncate(total_file_size)
+        return temp_output_file
---
+++ file_util.py
@@ -0,0 +1,87 @@
+
+from __future__ import division
+
+import base64
+import gzip
+import os
+import shutil
+import struct
+from io import open
+from logging import getLogger
+from Crypto.Hash import SHA256
+
+from .constants import UTF8
+
+class SnowflakeFileUtil(object):
+
+    @staticmethod
+    def compress_file_with_gzip(file_name, tmp_dir):
+        """
+        Compresses a file by GZIP
+        :param file_name: a file name
+        :param tmp_dir: temprary directory where an GZIP file will be created
+        :return: a paif of gzip file name and size
+        """
+        logger = getLogger(__name__)
+        base_name = os.path.basename(file_name)
+        gzip_file_name = os.path.join(tmp_dir, base_name + u'_c.gz')
+        logger.debug(u'gzip file: %s, original file: %s', gzip_file_name,
+                     file_name)
+        fr = open(file_name, u'rb')
+        fw = gzip.GzipFile(gzip_file_name, u'wb')
+        shutil.copyfileobj(fr, fw)
+        fw.close()
+        fr.close()
+        SnowflakeFileUtil.normalize_gzip_header(gzip_file_name)
+
+        statinfo = os.stat(gzip_file_name)
+        return gzip_file_name, statinfo.st_size
+
+
+    @staticmethod
+    def normalize_gzip_header(gzip_file_name):
+        """
+        Normalize GZIP file header. For consistent file digest, this removes
+        creation timestamp from the header.
+        :param gzip_file_name: gzip file name
+        """
+        logger = getLogger(__name__)
+        with open(gzip_file_name, u'r+b') as f:
+            # reset the timestamp in gzip header
+            f.seek(4, 0)
+            f.write(struct.pack('<L', 0))
+            # reset the file name in gzip header
+            f.seek(10, 0)
+            byte = f.read(1)
+            while byte:
+                value = struct.unpack('B', byte)[0]
+                # logger.debug(u'ch=%s, byte=%s', value, byte)
+                if value == 0:
+                    break
+                f.seek(-1, 1)  # current_pos - 1
+                f.write(struct.pack('B', 0x20))  # replace with a space
+                byte = f.read(1)
+
+    @staticmethod
+    def get_digest_and_size_for_file(file_name):
+        """
+        Gets file digest and size
+        :param file_name: a file name
+        :return:
+        """
+        CHUNK_SIZE = 16 * 4 * 1024
+        f = open(file_name, 'rb')
+        m = SHA256.new()
+        while True:
+            chunk = f.read(CHUNK_SIZE)
+            if chunk == b'':
+                break
+            m.update(chunk)
+
+        statinfo = os.stat(file_name)
+        file_size = statinfo.st_size
+        digest = base64.standard_b64encode(m.digest()).decode(UTF8)
+        logger = getLogger(__name__)
+        logger.debug(u'getting digest and size: %s, %s, file=%s', digest,
+                     file_size, file_name)
+        return digest, file_size
