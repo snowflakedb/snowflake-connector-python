199e8799bf59cd8ac9171731637211cf3fc7ce5b
Shige Takeda <smtakeda@gmail.com>
2018-01-11 20:36:43 +0000

    updated copyright year

--- test/test_unit_proxies.py
+++ test/test_unit_proxies.py
@@ -1,7 +1,7 @@
 # encoding=utf-8
 # !/usr/bin/env python
 #
-# Copyright (c) 2012-2017 Snowflake Computing Inc. All right reserved.
+# Copyright (c) 2012-2018 Snowflake Computing Inc. All right reserved.
 #
 
 
