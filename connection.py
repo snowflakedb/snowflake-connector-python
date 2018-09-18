6a168dead3b5b53dc1510779a02a609d5cc6f5bb
Boris Uvarov <boris.uwarow@gmail.com>
2018-09-18 19:02:01 +0300

    Use API call to issue a heartbeat, fix timeout value of timer

--- connection.py
+++ connection.py
@@ -923,8 +923,7 @@ class SnowflakeConnection(object):
     def add_heartbeat(self):
         """Add an hourly heartbeat query in order to keep connection alive."""
         if not self.heartbeat_thread:
-            self.heartbeat_thread = HourlyTimer(
-                self.heartbeat_tick)
+            self.heartbeat_thread = HourlyTimer(self.heartbeat_tick)
             self.heartbeat_thread.start()
 
     def cancel_heartbeat(self):
@@ -937,8 +936,7 @@ class SnowflakeConnection(object):
     def heartbeat_tick(self):
         """Execute a hearbeat query if connection isn't closed yet."""
         if not self.is_closed():
-            cursor = self.cursor()
-            cursor.execute('SELECT 1;')
+            self.execute_string("SELECT 1;")
 
     def __enter__(self):
         u"""
