5831904927aa924b2b16beb43c827dc1f5f97f49
Shige Takeda <smtakeda@gmail.com>
2018-09-21 23:49:39 +0000

    Updated the test case for datetime binding in qmark mode

--- test/test_qmark.py
+++ test/test_qmark.py
@@ -68,7 +68,6 @@ def test_qmark_paramstyle_enabled(conn_cnx, db_parameters):
     """
     Enable qmark binding
     """
-    import datetime
     import snowflake.connector
     snowflake.connector.paramstyle = u'qmark'
     try:
@@ -89,25 +88,6 @@ def test_qmark_paramstyle_enabled(conn_cnx, db_parameters):
             cnx.cursor().execute(
                 "DROP TABLE IF EXISTS {name}".format(
                     name=db_parameters['name']))
-    try:
-        with conn_cnx() as cnx:
-            cnx.cursor().execute(
-                "CREATE OR REPLACE TABLE {name} "
-                "(aa TIMESTAMP)".format(
-                    name=db_parameters['name']))
-            inserts = tuple([(datetime.datetime(2018, 1, i + 1),) for i in range(2)])
-            cnx.cursor().executemany(
-                "INSERT INTO {name} VALUES(?)".format(
-                    name=db_parameters['name']),
-                inserts)
-            for i, rec in enumerate(cnx.cursor().execute(
-                    "SELECT * FROM {name}".format(name=db_parameters['name']))):
-                assert rec[0] == inserts[i][0], "First column value"
-    finally:
-        with conn_cnx() as cnx:
-            cnx.cursor().execute(
-                "DROP TABLE IF EXISTS {name}".format(
-                    name=db_parameters['name']))
         snowflake.connector.paramstyle = u'pyformat'
 
     # After changing back to pyformat, binding qmark should fail.
@@ -121,6 +101,38 @@ def test_qmark_paramstyle_enabled(conn_cnx, db_parameters):
                 cnx.cursor().execute(
                     "INSERT INTO {name} VALUES(?, ?)".format(
                         name=db_parameters['name']), ('test11', 'test12'))
+    finally:
+        with conn_cnx() as cnx:
+            cnx.cursor().execute(
+                "DROP TABLE IF EXISTS {name}".format(
+                    name=db_parameters['name']))
+
+
+def test_binding_datetime_qmark(conn_cnx, db_parameters):
+    """
+    Ensures datetime can bind
+    """
+    import datetime
+    import snowflake.connector
+    snowflake.connector.paramstyle = u'qmark'
+    try:
+        with conn_cnx() as cnx:
+            cnx.cursor().execute(
+                "CREATE OR REPLACE TABLE {name} "
+                "(aa TIMESTAMP_NTZ)".format(
+                    name=db_parameters['name']))
+            days = 2
+            inserts = tuple(
+                [(datetime.datetime(2018, 1, i + 1),) for i in range(days)])
+            cnx.cursor().executemany(
+                "INSERT INTO {name} VALUES(?)".format(
+                    name=db_parameters['name']),
+                inserts)
+            ret = cnx.cursor().execute(
+                "SELECT * FROM {name} ORDER BY 1".format(
+                    name=db_parameters['name'])).fetchall()
+            for i in range(days):
+                assert ret[i][0] == inserts[i][0]
     finally:
         with conn_cnx() as cnx:
             cnx.cursor().execute(
