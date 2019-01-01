12ec1c43f54dcc51b43f001d18dade51bcb871fa
Shige Takeda <smtakeda@gmail.com>
2019-01-01 04:44:19 +0000

    Added Supported Python version 3.7

--- setup.py
+++ setup.py
@@ -102,6 +102,7 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
 
         'Topic :: Database',
         'Topic :: Software Development',
