a5c48faba2159170c00a902c060fd2ba45d89d26
Shige Takeda <smtakeda@gmail.com>
2019-01-01 02:09:18 +0000

    added 3.7

---
+++ scripts/install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -e
+#
+# Install Snowflake Python Connector
+#
+set -o pipefail
+sudo apt-get update
+openssl aes-256-cbc -k "$super_secret_password" -in parameters.py.enc -out test/parameters.py -d
+curl -O https://bootstrap.pypa.io/get-pip.py
+python get-pip.py
+pip --version
+pip install -U virtualenv
+python -m virtualenv venv
+source ./venv/bin/activate
+pip install pytest-rerunfailures
+if [[ "$TRAVIS_PYTHON_VERSION" == "2.7" ]]; then
+    pip install mock
+fi
+pip install .
+pip list --format=columns
