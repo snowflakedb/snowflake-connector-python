7765a740155deab9cb9fc8abaf510ccc22255326
Shige Takeda <smtakeda@gmail.com>
2019-02-22 01:53:07 +0000

    SNOW-66025: skip HEAD when OVERWRITE=true for PUT

--- remote_storage_util.py
+++ remote_storage_util.py
@@ -12,11 +12,10 @@ import time
 from collections import namedtuple
 from logging import getLogger
 
-from .s3_util import SnowflakeS3Util
 from .azure_util import SnowflakeAzureUtil
-
 from .constants import (SHA256_DIGEST, ResultStatus)
 from .encryption_util import (SnowflakeEncryptionUtil)
+from .s3_util import SnowflakeS3Util
 
 DEFAULT_CONCURRENCY = 1
 DEFAULT_MAX_RETRY = 5
@@ -41,15 +40,18 @@ class SnowflakeRemoteStorageUtil(object):
 
     @staticmethod
     def getStorageType(type):
-        if(type == u'S3'):
+        if (type == u'S3'):
             return SnowflakeS3Util
-        elif(type == u'AZURE'):
+        elif (type == u'AZURE'):
             return SnowflakeAzureUtil
 
     @staticmethod
     def create_client(stage_info, use_accelerate_endpoint=False):
-        util_class = SnowflakeRemoteStorageUtil.getStorageType(stage_info[u'locationType'])
-        return util_class.create_client(stage_info, use_accelerate_endpoint=False)
+        util_class = SnowflakeRemoteStorageUtil.getStorageType(
+            stage_info[u'locationType'])
+        return util_class.create_client(
+            stage_info,
+            use_accelerate_endpoint=use_accelerate_endpoint)
 
     @staticmethod
     def upload_one_file_to_s3(meta):
@@ -61,7 +63,8 @@ class SnowflakeRemoteStorageUtil(object):
         encryption_metadata = None
 
         if u'encryption_material' in meta:
-            (encryption_metadata, data_file) = SnowflakeEncryptionUtil.encrypt_file(
+            (encryption_metadata,
+             data_file) = SnowflakeEncryptionUtil.encrypt_file(
                 meta[u'encryption_material'],
                 meta[u'real_src_file_name'], tmp_dir=meta[u'tmp_dir'])
 
@@ -71,32 +74,40 @@ class SnowflakeRemoteStorageUtil(object):
         else:
             logger.debug(u'not encrypted data file')
             data_file = meta[u'real_src_file_name']
-        util_class = SnowflakeRemoteStorageUtil.getStorageType(meta[u'stage_info'][u'locationType'])
-        file_header = util_class.get_file_header(meta, meta[u'dst_file_name'])
-        if meta[u'result_status'] == ResultStatus.RENEW_TOKEN:
-            # need renew token
-            return
-        elif file_header and meta[u'result_status'] == ResultStatus.UPLOADED and \
-                not meta.get(u'overwrite'):
-            logger.debug(
-                u'file already exists, checking digest: location="%s", file_name="%s"',
-                meta[u'stage_info'][u'location'], meta[u'dst_file_name'])
-            sfc_digest = file_header.digest
-            if sfc_digest == meta[SHA256_DIGEST]:
-                logger.debug(u'file digest matched: digest=%s',
-                             sfc_digest)
-                meta[u'dst_file_size'] = 0
-                meta[u'error_details'] = \
-                    (u'File with the same destination name '
-                     u'and checksum already exists')
-                meta[u'result_status'] = ResultStatus.SKIPPED
+        util_class = SnowflakeRemoteStorageUtil.getStorageType(
+            meta[u'stage_info'][u'locationType'])
+        if not meta.get(u'overwrite'):
+            file_header = util_class.get_file_header(
+                meta, meta[u'dst_file_name'])
+            if meta[u'result_status'] == ResultStatus.RENEW_TOKEN:
+                # need renew token
                 return
-            else:
+            elif file_header and meta[
+                u'result_status'] == ResultStatus.UPLOADED and \
+                    not meta.get(u'overwrite'):
                 logger.debug(
-                    u"file digest didn't match: digest_s3=%s, digest_local=%s",
-                    sfc_digest, meta[SHA256_DIGEST])
+                    u'file already exists, checking digest: '
+                    u'location="%s", file_name="%s"',
+                    meta[u'stage_info'][u'location'],
+                    meta[u'dst_file_name'])
+                sfc_digest = file_header.digest
+                if sfc_digest == meta[SHA256_DIGEST]:
+                    logger.debug(u'file digest matched: digest=%s',
+                                 sfc_digest)
+                    meta[u'dst_file_size'] = 0
+                    meta[u'error_details'] = \
+                        (u'File with the same destination name '
+                         u'and checksum already exists')
+                    meta[u'result_status'] = ResultStatus.SKIPPED
+                    return
+                else:
+                    logger.debug(
+                        u"file digest didn't match: digest_s3=%s, "
+                        u"digest_local=%s",
+                        sfc_digest, meta[SHA256_DIGEST])
 
-        logger.debug(u'putting a file: %s, %s', meta[u'stage_info'][u'location'], meta[u'dst_file_name'])
+        logger.debug(u'putting a file: %s, %s',
+                     meta[u'stage_info'][u'location'], meta[u'dst_file_name'])
 
         max_concurrency = meta[u'parallel']
         last_err = None
@@ -109,11 +120,11 @@ class SnowflakeRemoteStorageUtil(object):
                 max_concurrency
             )
 
-            if(meta[u'result_status'] == ResultStatus.UPLOADED):
+            if (meta[u'result_status'] == ResultStatus.UPLOADED):
                 return
             elif (meta[u'result_status'] == ResultStatus.RENEW_TOKEN):
                 return
-            elif(meta[u'result_status'] == ResultStatus.NEED_RETRY):
+            elif (meta[u'result_status'] == ResultStatus.NEED_RETRY):
                 last_err = meta[u'last_error']
                 logger.debug(
                     'Failed to upload a file: %s, err: %s. Retrying with '
@@ -123,7 +134,8 @@ class SnowflakeRemoteStorageUtil(object):
                     sleeping_time = min(2 ** retry, 16)
                     logger.debug(u"sleeping: %s", sleeping_time)
                     time.sleep(sleeping_time)
-            elif(meta[u'result_status'] == ResultStatus.NEED_RETRY_WITH_LOWER_CONCURRENCY):
+            elif (meta[
+                      u'result_status'] == ResultStatus.NEED_RETRY_WITH_LOWER_CONCURRENCY):
                 last_err = meta[u'last_error']
                 max_concurrency = meta[u'parallel'] - int(
                     retry * meta[u'parallel'] / max_retry)
@@ -162,7 +174,8 @@ class SnowflakeRemoteStorageUtil(object):
         if not os.path.exists(base_dir):
             os.makedirs(base_dir)
 
-        util_class = SnowflakeRemoteStorageUtil.getStorageType(meta[u'stage_info'][u'locationType'])
+        util_class = SnowflakeRemoteStorageUtil.getStorageType(
+            meta[u'stage_info'][u'locationType'])
         file_header = util_class.get_file_header(meta, meta[u'src_file_name'])
         meta[u'src_file_size'] = file_header.content_length
 
@@ -175,13 +188,15 @@ class SnowflakeRemoteStorageUtil(object):
         last_err = None
         max_retry = DEFAULT_MAX_RETRY
         for retry in range(max_retry):
-            util_class._native_download_file(meta, full_dst_file_name, max_concurrency)
-            if(meta[u'result_status'] == ResultStatus.DOWNLOADED):
+            util_class._native_download_file(meta, full_dst_file_name,
+                                             max_concurrency)
+            if (meta[u'result_status'] == ResultStatus.DOWNLOADED):
                 if u'encryption_material' in meta:
                     logger.debug(
                         u'encrypted data file=%s', full_dst_file_name)
                     tmp_dst_file_name = SnowflakeEncryptionUtil.decrypt_file(
-                        file_header.encryption_metadata, meta[u'encryption_material'],
+                        file_header.encryption_metadata,
+                        meta[u'encryption_material'],
                         full_dst_file_name, tmp_dir=meta[u'tmp_dir'])
                     shutil.copyfile(tmp_dst_file_name, full_dst_file_name)
                     os.unlink(tmp_dst_file_name)
@@ -194,7 +209,8 @@ class SnowflakeRemoteStorageUtil(object):
                 return
             elif (meta[u'result_status'] == ResultStatus.RENEW_TOKEN):
                 return
-            elif (meta[u'result_status'] == ResultStatus.NEED_RETRY_WITH_LOWER_CONCURRENCY):
+            elif (meta[
+                      u'result_status'] == ResultStatus.NEED_RETRY_WITH_LOWER_CONCURRENCY):
                 max_concurrency = meta[u'parallel'] - int(
                     retry * meta[u'parallel'] / max_retry)
                 max_concurrency = max(DEFAULT_CONCURRENCY, max_concurrency)
@@ -208,7 +224,7 @@ class SnowflakeRemoteStorageUtil(object):
                     sleeping_time = min(2 ** retry, 16)
                     logger.debug(u"sleeping: %s", sleeping_time)
                     time.sleep(sleeping_time)
-            elif(meta[u'result_status'] == ResultStatus.NEED_RETRY):
+            elif (meta[u'result_status'] == ResultStatus.NEED_RETRY):
                 last_err = meta[u'last_error']
                 logger.debug(
                     'Failed to download a file: %s, err: %s. Retrying with '
@@ -234,7 +250,8 @@ class SnowflakeRemoteStorageUtil(object):
         """
         logger = getLogger(__name__)
 
-        util_class = SnowflakeRemoteStorageUtil.getStorageType(meta[u'stage_info'][u'locationType'])
+        util_class = SnowflakeRemoteStorageUtil.getStorageType(
+            meta[u'stage_info'][u'locationType'])
         for _ in range(10):
             # retry
             SnowflakeRemoteStorageUtil.upload_one_file_to_s3(meta)
