c574d396dd2bfc7c6911478d9ac673897524f679
Shige Takeda <shige.takeda@snowflake.com>
2019-10-20 17:40:40 -0400

    SNOW-78638: Do not send snowflake python connector telemetry during interpreter shutdown

--- telemetry.py
+++ telemetry.py
@@ -108,10 +108,11 @@ class TelemetryClient(object):
     def is_closed(self):
         return self._is_closed
 
-    def close(self):
+    def close(self, send_on_close=True):
         if not self._is_closed:
             logger.debug("Closing telemetry client.")
-            self.send_batch()
+            if send_on_close:
+                self.send_batch()
             self._is_closed = True
 
     def disable(self):
