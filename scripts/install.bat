46d57be7d6759ea6e442d62c34e09628f8db7c33
Haowei Yu <yuhaowei@umich.edu>
2019-10-25 22:49:58 -0700

    Enable arrow tests on windows

--- scripts/install.bat
+++ scripts/install.bat
@@ -2,6 +2,8 @@ nuget install secure-file -ExcludeVersion
 secure-file\tools\secure-file -decrypt parameters.appveyor.py.enc -secret %my_secret% -out parameters.py
 copy parameters.py test
 
+SET SCRIPT_DIR=%~dp0
+
 "%PYTHON%/python.exe" -m venv env
 call env\Scripts\activate
 # https://github.com/pypa/pip/issues/6566
@@ -11,5 +13,16 @@ pip install numpy
 pip install pendulum
 pip install pyarrow
 pip install pytest pytest-cov pytest-rerunfailures
-pip install .
+pip install wheel
+pip install Cython
+set ENABLE_EXT_MODULES=true
+python setup.py bdist_wheel -d dist
+
+:: figure out connector wheel file name
+cd dist
+dir /b * > whl_name
+set /p connector_whl=<whl_name
+pip install %connector_whl%
 pip list --format=columns
+
+cd %SCRIPT_DIR%/..
