7a68204b83c991a34d31295d49d9331bd9c3f8ec
Haowei Yu <yuhaowei@umich.edu>
2019-10-25 17:37:29 -0700

    Enable arrow tests in linux

---
+++ scripts/build_connector_linux.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -e
+#
+# Install Snowflake Python Connector
+#
+set -o pipefail
+
+THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cd $THIS_DIR/..
+PYTHON_VERSION=$1
+source "/home/user/venv-build-${PYTHON_VERSION}/bin/activate"
+rm -rf build/
+export ENABLE_EXT_MODULES=true
+rm -f generated_version.py
+CC=g++ python setup.py bdist_wheel -d $THIS_DIR/../dist/docker/$PYTHON_VERSION/
+unset ENABLE_EXT_MODULES
+
+mkdir -p $THIS_DIR/../dist/docker/repaired_wheels
+auditwheel repair --plat manylinux2010_x86_64 -L connector $THIS_DIR/../dist/docker/$PYTHON_VERSION/*.whl -w $THIS_DIR/../dist/docker/repaired_wheels
+rm $THIS_DIR/../dist/docker/repaired_wheels/*manylinux1_x86_64.whl || true
