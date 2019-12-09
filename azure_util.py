ba691f8b526f201059bc67c67177ce35964c4db2
Shige Takeda <shige.takeda@snowflake.com>
2019-12-09 10:51:05 -0800

    SNOW-116535: fix GET command for Azure for workaround https://github.com/Azure/azure-storage-python/issues/509

--- azure_util.py
+++ azure_util.py
@@ -14,6 +14,13 @@ from .constants import (
     SHA256_DIGEST, ResultStatus, FileHeader, HTTP_HEADER_VALUE_OCTET_STREAM)
 from .encryption_util import (EncryptionMetadata)
 
+from azure.storage.common._http.httpclient import (
+    _HTTPClient,
+    HTTPResponse,
+)
+from azure.storage.common._serialization import _get_data_bytes_or_stream_only
+import requests
+
 logger = getLogger(__name__)
 
 """
@@ -52,6 +59,7 @@ class SnowflakeAzureUtil(object):
         client = BlockBlobService(account_name=stage_info[u'storageAccount'],
                                   sas_token=sas_token,
                                   endpoint_suffix=end_point)
+        client._httpclient = RawBodyReadingClient(session=requests.session(), protocol="https", timeout=2000)
         client.retry = ExponentialRetry(
             initial_backoff=1, increment_base=2, max_attempts=60, random_jitter_range=2).retry
         return client
@@ -259,3 +267,54 @@ class SnowflakeAzureUtil(object):
                 meta[u'result_status'] = ResultStatus.NEED_RETRY
         # Comparing with s3, azure haven't experienced OpenSSL.SSL.SysCallError,
         # so we will add logic to catch it only when it happens
+
+
+class RawBodyReadingClient(_HTTPClient):
+    '''
+    This class is needed because the Azure storage Python library has a bug that doesn't
+    allow it to download files with content-encoding=gzip compressed. See
+    https://github.com/Azure/azure-storage-python/issues/509. This client overrides the
+    default HTTP client and downloads uncompressed. This workaround was provided by
+    Microsoft.
+    '''
+    def perform_request(self, request):
+        '''
+        Sends an HTTPRequest to Azure Storage and returns an HTTPResponse. If
+        the response code indicates an error, raise an HTTPError.
+
+        :param HTTPRequest request:
+            The request to serialize and send.
+        :return: An HTTPResponse containing the parsed HTTP response.
+        :rtype: :class:`~azure.storage.common._http.HTTPResponse`
+        '''
+        # Verify the body is in bytes or either a file-like/stream object
+        if request.body:
+            request.body = _get_data_bytes_or_stream_only('request.body', request.body)
+
+        # Construct the URI
+        uri = self.protocol.lower() + '://' + request.host + request.path
+
+        # Send the request
+        response = self.session.request(request.method,
+                                        uri,
+                                        params=request.query,
+                                        headers=request.headers,
+                                        data=request.body or None,
+                                        timeout=self.timeout,
+                                        proxies=self.proxies,
+                                        stream=True)
+
+        # Parse the response
+        status = int(response.status_code)
+        response_headers = {}
+        for key, name in response.headers.items():
+            # Preserve the case of metadata
+            if key.lower().startswith('x-ms-meta-'):
+                response_headers[key] = name
+            else:
+                response_headers[key.lower()] = name
+
+        wrap = HTTPResponse(status, response.reason, response_headers, response.raw.read())
+        response.close()
+
+        return wrap
