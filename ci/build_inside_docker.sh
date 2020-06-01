3b54652952ed89f8d35965c06efa33bef959c161
Shige Takeda <shige.takeda@snowflake.com>
2020-06-01 14:59:03 +0000

    SNOW-161936 updated the image name with the internal docker registry

--- ci/build_inside_docker.sh
+++ ci/build_inside_docker.sh
@@ -5,11 +5,12 @@
 set -o pipefail
 
 THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source $THIS_DIR/set_base_image.sh
 
 cd $THIS_DIR/../docker/manylinux2010
 
 echo "[Info] Start building dokcer image"
-docker build -t manylinux:1.0 -f Dockerfile-x86_64_base .
+docker build -t manylinux:1.0 --build-arg BASE_IMAGE=$BASE_IMAGE_MANYLINUX1 -f Dockerfile-x86_64_base .
 
 user_id=$(id -u $USER)
 docker run -e LOCAL_USER_ID=$user_id --mount type=bind,source="$THIS_DIR/..",target=/home/user/connector manylinux:1.0 \
