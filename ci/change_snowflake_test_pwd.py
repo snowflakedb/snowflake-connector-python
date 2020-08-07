482f6de0039d401db09b2d7247432aaa5c827ad3
wshangguan <wufan.shangguan@snowflake.com>
2020-08-07 21:31:08 +0000

    fix_lint bug

--- ci/change_snowflake_test_pwd.py
+++ ci/change_snowflake_test_pwd.py
@@ -5,40 +5,51 @@
 import os
 import sys
 
-from jenkins_test_parameters import SNOWFLAKE_TEST_PASSWORD_NEW
-
 import snowflake.connector
-from parameters import CONNECTION_PARAMETERS
 
 sys.path.append(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'test'))
 
-params = {
-    'account': '<account_name>',
-    'user': '<user_name>',
-    'password': '<password>',
-    'database': '<database_name>',
-    'schema': '<schema_name>',
-    'protocol': 'https',
-    'host': '<host>',
-    'port': '443',
-}
+CLIENT_KNOWN_SSM_FILE_PATH_DOCKER = 'CLIENT_KNOWN_SSM_FILE_PATH_DOCKER'
 
-# do we need to set time zone?
-# import time
-# os.environ['TZ'] = 'UTC'
-# if not IS_WINDOWS:
-#     time.tzset()
 
-for k, v in CONNECTION_PARAMETERS.items():
-    params[k] = v
+def change_password():
+    params = {
+        'account': '<account_name>',
+        'user': '<user_name>',
+        'password': '<password>',
+        'database': '<database_name>',
+        'schema': '<schema_name>',
+        'protocol': 'https',
+        'host': '<host>',
+        'port': '443',
+    }
 
-conn = snowflake.connector.connect(**params)
-conn.cursor().execute("use role accountadmin")
-cmd = "alter user set password = '{}'".format(SNOWFLAKE_TEST_PASSWORD_NEW)
-print(cmd)
-conn.cursor().execute(cmd)
-conn.close()
+    # do we need to set time zone?
+    # import time
+    # os.environ['TZ'] = 'UTC'
+    # if not IS_WINDOWS:
+    #     time.tzset()
 
-# generate ssm file
-with open(os.getenv('CLIENT_KNOWN_SSM_FILE_PATH_DOCKER'), 'w') as f:
-    f.write(SNOWFLAKE_TEST_PASSWORD_NEW + '\n')
+    from parameters import CONNECTION_PARAMETERS
+
+    for k, v in CONNECTION_PARAMETERS.items():
+        params[k] = v
+
+    conn = snowflake.connector.connect(**params)
+    conn.cursor().execute("use role accountadmin")
+    cmd = "alter user set password = '{}'".format(SNOWFLAKE_TEST_PASSWORD_NEW)
+    print(cmd)
+    conn.cursor().execute(cmd)
+    conn.close()
+
+
+def generate_known_ssm_file():
+    from jenkins_test_parameters import SNOWFLAKE_TEST_PASSWORD_NEW
+    # generate ssm file
+    with open(os.getenv(CLIENT_KNOWN_SSM_FILE_PATH_DOCKER), 'w') as f:
+        f.write(SNOWFLAKE_TEST_PASSWORD_NEW + '\n')
+
+
+if __name__ == '__main__':
+    change_password()
+    generate_known_ssm_file()
