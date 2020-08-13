1766c239f8f2d9f833d30d4cdd5081d7fcdc1127
wshangguan <wufan.shangguan@snowflake.com>
2020-08-13 22:19:36 +0000

    more debug info

--- test_util.py
+++ test_util.py
@@ -11,6 +11,11 @@ RUNNING_ON_GH = os.getenv('GITHUB_ACTIONS') == 'true'
 REGRESSION_TEST_LOG_DIR = os.getenv('CLIENT_LOG_DIR_PATH_DOCKER', str(pathlib.Path(__file__).parent.absolute()))
 rt_plain_logger = None
 
+print("[WUFAN DEBUG] GITHUB_ACTIONS is {}, type of it is {}".format(os.getenv('GITHUB_ACTIONS'), type(os.getenv('GITHUB_ACTIONS'))))
+print("[WUFAN DEBUG] RUNNING_ON_GH is {}".format(RUNNING_ON_GH))
+print("[WUFAN DEBUG] CLIENT_LOG_DIR_PATH_DOCKER is {}".format(os.getenv('CLIENT_LOG_DIR_PATH_DOCKER')))
+print("[WUFAN DEBUG] REGRESSION_TEST_LOG_DIR is {}".format(REGRESSION_TEST_LOG_DIR))
+
 if RUNNING_ON_GH:
     rt_plain_logger = logging.getLogger('regression.test.plain.logger')
     rt_plain_logger.setLevel(logging.DEBUG)
