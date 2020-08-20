793ae4747cea6eabdf643e83860fc6cf363b48e8
wshangguan <wufan.shangguan@snowflake.com>
2020-08-20 00:45:16 +0000

    change telemetry log path and only generate on Linux

--- src/snowflake/connector/telemetry.py
+++ src/snowflake/connector/telemetry.py
@@ -7,7 +7,7 @@ import logging
 from threading import Lock
 
 from .secret_detector import SecretDetector
-from .test_util import RUNNING_ON_JENKINS, rt_plain_logger
+from .test_util import ENABLE_TELEMETRY_LOG, rt_plain_logger
 
 logger = logging.getLogger(__name__)
 
@@ -88,7 +88,7 @@ class TelemetryClient(object):
 
         body = {'logs': [x.to_dict() for x in to_send]}
         logger.debug("Sending %d logs to telemetry. Data is %s.", len(body), SecretDetector.mask_secrets(str(body)))
-        if RUNNING_ON_JENKINS:
+        if ENABLE_TELEMETRY_LOG:
             # This logger guarantees the payload won't be masked. Testing purpose.
             rt_plain_logger.debug("Inband telemetry data being sent is {}".format(body))
         try:
