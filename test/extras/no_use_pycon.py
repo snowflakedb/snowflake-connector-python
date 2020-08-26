c761502e3acd395038141d6f8ed87e650680f20b
Mark Keller <mark.keller@snowflake.com>
2020-08-26 06:27:40 +0000

    adding extras tests

---
+++ test/extras/no_use_pycon.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2012-2019 Snowflake Computing Inc. All right reserved.
+#
+# This test tests for SNOW-186747
+
+import requests
+
+import snowflake.connector  # NOQA
+
+r = requests.get('https://snowflake.com/', headers={"User-Agent": ""}, allow_redirects=False)
+assert r.status_code == 301
