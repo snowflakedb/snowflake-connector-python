8dfed78c5c76ed69114c9bbfe3a48a5b1c31b2c3
Kyle Flanagan <kyle.flanagan@agoradata.com>
2020-09-17 16:05:37 -0500

    Consolidate tests

--- test/pandas/test_pandas_tools.py
+++ test/pandas/test_pandas_tools.py
@@ -1,4 +1,5 @@
 import math
+from datetime import datetime
 from typing import Callable, Dict, Generator
 
 import mock
@@ -26,18 +27,48 @@ sf_connector_version_df = pandas.DataFrame(sf_connector_version_data, columns=['
 @pytest.mark.parametrize('compression', ['gzip', 'snappy'])
 # Note: since the file will to small to chunk, this is only testing the put command's syntax
 @pytest.mark.parametrize('parallel', [4, 99])
-def test_write_pandas_quoted_identifiers(conn_cnx: Callable[..., Generator['SnowflakeConnection', None, None]],
+@pytest.mark.parametrize('quote_identifiers', [True, False])
+@pytest.mark.parametrize('create_default_columns', [True, False])
+def test_write_pandas(conn_cnx: Callable[..., Generator['SnowflakeConnection', None, None]],
                       db_parameters: Dict[str, str],
                       compression: str,
                       parallel: int,
-                      chunk_size: int):
+                      chunk_size: int,
+                      quote_identifiers: bool,
+                      create_default_columns: bool):
     num_of_chunks = math.ceil(len(sf_connector_version_data) / chunk_size)
 
     with conn_cnx(user=db_parameters['user'],
                   account=db_parameters['account'],
                   password=db_parameters['password']) as cnx:  # type: SnowflakeConnection
         table_name = 'driver_versions'
-        cnx.execute_string('CREATE OR REPLACE TABLE "{}"("name" STRING, "newest_version" STRING)'.format(table_name))
+
+        if quote_identifiers:
+            if create_default_columns:
+                create_sql = """CREATE OR REPLACE TABLE "{}" 
+                             ("name" STRING, "newest_version" STRING,
+                             "id" varchar(36) default uuid_string(),
+                             "ts" timestamp_ltz default current_timestamp)""".format(table_name)
+                id_key = 'id'
+                ts_key = 'ts'
+            else:
+                create_sql = 'CREATE OR REPLACE TABLE "{}" ("name" STRING, "newest_version" STRING)'.format(table_name)
+            select_sql = 'SELECT * FROM "{}"'.format(table_name)
+            drop_sql = 'DROP TABLE IF EXISTS "{}"'.format(table_name)
+        else:
+            if create_default_columns:
+                create_sql = """CREATE OR REPLACE TABLE {} 
+                             (name STRING, newest_version STRING,
+                             id varchar(36) default uuid_string(),
+                             ts timestamp_ltz default current_timestamp)""".format(table_name)
+                id_key = 'ID'
+                ts_key = 'TS'
+            else:
+                create_sql = 'CREATE OR REPLACE TABLE {} (name STRING, newest_version STRING)'.format(table_name)
+            select_sql = 'SELECT * FROM {}'.format(table_name)
+            drop_sql = 'DROP TABLE IF EXISTS {}'.format(table_name)
+
+        cnx.execute_string(create_sql)
         try:
             success, nchunks, nrows, _ = write_pandas(cnx,
                                                       sf_connector_version_df,
@@ -45,15 +76,25 @@ def test_write_pandas_quoted_identifiers(conn_cnx: Callable[..., Generator['Snow
                                                       compression=compression,
                                                       parallel=parallel,
                                                       chunk_size=chunk_size,
-                                                      quote_identifiers=True)
-            if num_of_chunks == 1:
+                                                      quote_identifiers=quote_identifiers)
+            
+            if create_default_columns:
+                from snowflake.connector import DictCursor
+                result = cnx.cursor(DictCursor).execute(select_sql).fetchall()
+                for row in result:
+                    assert row[id_key] is not None  # ID (UUID String)
+                    assert len(row[id_key]) == 36
+                    assert row[ts_key] is not None  # TS (Current Timestamp)
+                    assert isinstance(row[ts_key], datetime)
+            elif num_of_chunks == 1:
                 # Note: since we used one chunk order is conserved
-                assert (cnx.cursor().execute('SELECT * FROM "{}"'.format(table_name)).fetchall() ==
+                assert (cnx.cursor().execute(select_sql).fetchall() ==
                         sf_connector_version_data)
             else:
                 # Note: since we used one chunk order is NOT conserved
-                assert (set(cnx.cursor().execute('SELECT * FROM "{}"'.format(table_name)).fetchall()) ==
+                assert (set(cnx.cursor().execute(select_sql).fetchall()) ==
                         set(sf_connector_version_data))
+                
             # Make sure all files were loaded and no error occurred
             assert success
             # Make sure overall as many rows were ingested as we tried to insert
@@ -61,190 +102,68 @@ def test_write_pandas_quoted_identifiers(conn_cnx: Callable[..., Generator['Snow
             # Make sure we uploaded in as many chunk as we wanted to
             assert nchunks == num_of_chunks
         finally:
-            cnx.execute_string("DROP TABLE IF EXISTS {}".format(table_name))
+            cnx.execute_string(drop_sql)
 
 
-@pytest.mark.parametrize('chunk_size', [5, 4, 3, 2, 1])
-@pytest.mark.parametrize('compression', ['gzip', 'snappy'])
-# Note: since the file will be too small to chunk, this is only testing the put command's syntax
-@pytest.mark.parametrize('parallel', [4, 99])
-def test_write_pandas(conn_cnx: Callable[..., Generator['SnowflakeConnection', None, None]],
-                      db_parameters: Dict[str, str],
-                      compression: str,
-                      parallel: int,
-                      chunk_size: int):
-    num_of_chunks = math.ceil(len(sf_connector_version_data) / chunk_size)
-
-    with conn_cnx(user=db_parameters['user'],
-                  account=db_parameters['account'],
-                  password=db_parameters['password']) as cnx:  # type: SnowflakeConnection
-        table_name = 'driver_versions'
-        # by default (quote_identifiers=False), the user is not interested in quoting identifiers, so do
-        # not quote them when creating table
-        cnx.execute_string('CREATE OR REPLACE TABLE {} (name STRING, newest_version STRING)'.format(table_name))
-        try:
-            success, nchunks, nrows, _ = write_pandas(cnx,
-                                                      sf_connector_version_df,
-                                                      table_name,
-                                                      compression=compression,
-                                                      parallel=parallel,
-                                                      chunk_size=chunk_size)
-            if num_of_chunks == 1:
-                # Note: since we used one chunk order is conserved
-                assert (cnx.cursor().execute('SELECT * FROM {}'.format(table_name)).fetchall() ==
-                        sf_connector_version_data)
-            else:
-                # Note: since we used one chunk order is NOT conserved
-                assert (set(cnx.cursor().execute('SELECT * FROM {}'.format(table_name)).fetchall()) ==
-                        set(sf_connector_version_data))
-            # Make sure all files were loaded and no error occurred
-            assert success
-            # Make sure overall as many rows were ingested as we tried to insert
-            assert nrows == len(sf_connector_version_data)
-            # Make sure we uploaded in as many chunk as we wanted to
-            assert nchunks == num_of_chunks
-        finally:
-            cnx.execute_string("DROP TABLE IF EXISTS {}".format(table_name))
-
-
-@pytest.mark.parametrize('chunk_size', [5, 4, 3, 2, 1])
-@pytest.mark.parametrize('compression', ['gzip', 'snappy'])
-# Note: since the file will to small to chunk, this is only testing the put command's syntax
-@pytest.mark.parametrize('parallel', [4, 99])
-def test_write_pandas_default_cols(conn_cnx: Callable[..., Generator['SnowflakeConnection', None, None]],
-                      db_parameters: Dict[str, str],
-                      compression: str,
-                      parallel: int,
-                      chunk_size: int):
-    from snowflake.connector import DictCursor
-    num_of_chunks = math.ceil(len(sf_connector_version_data) / chunk_size)
-
-    with conn_cnx(user=db_parameters['user'],
-                  account=db_parameters['account'],
-                  password=db_parameters['password']) as cnx:  # type: SnowflakeConnection
-        table_name = 'driver_versions'
-        # by default (quote_identifiers=False), the user is not interested in quoting identifiers, so do
-        # not quote them when creating table
-        cnx.execute_string("""CREATE OR REPLACE TABLE {} (
-                              id varchar(36) default uuid_string(),
-                              name STRING, newest_version STRING,
-                              ts timestamp_ltz default current_timestamp)""".format(table_name))
-        try:
-            success, nchunks, nrows, _ = write_pandas(cnx,
-                                                      sf_connector_version_df,
-                                                      table_name,
-                                                      compression=compression,
-                                                      parallel=parallel,
-                                                      chunk_size=chunk_size)
-            result = cnx.cursor(DictCursor).execute('SELECT * FROM {}'.format(table_name)).fetchall()
-            for row in result:
-                assert row['ID'] is not None
-                assert row['TS'] is not None
-            # Make sure all files were loaded and no error occurred
-            assert success
-            # Make sure overall as many rows were ingested as we tried to insert
-            assert nrows == len(sf_connector_version_data)
-            # Make sure we uploaded in as many chunk as we wanted to
-            assert nchunks == num_of_chunks
-        finally:
-            cnx.execute_string("DROP TABLE IF EXISTS {}".format(table_name))
-
-
-def test_location_building_db_schema(conn_cnx):
+@pytest.mark.parametrize('quote_identifiers', [True, False])
+def test_location_building_db_schema(conn_cnx, quote_identifiers: bool):
     """This tests that write_pandas constructs location correctly with database, schema and table name."""
     from snowflake.connector.cursor import SnowflakeCursor
     with conn_cnx() as cnx:  # type: SnowflakeConnection
         def mocked_execute(*args, **kwargs):
             if len(args) >= 1 and args[0].startswith('COPY INTO'):
                 location = args[0].split(' ')[2]
-                assert location == 'database.schema.table'
+                if quote_identifiers:
+                    assert location == '"database"."schema"."table"'
+                else:
+                    assert location == 'database.schema.table'
             cur = SnowflakeCursor(cnx)
             cur._result = iter([])
             return cur
         with mock.patch('snowflake.connector.cursor.SnowflakeCursor.execute', side_effect=mocked_execute) as m_execute:
             success, nchunks, nrows, _ = write_pandas(cnx, sf_connector_version_df, "table",
-                                                      database='database', schema='schema')
+                                                      database='database', schema='schema', 
+                                                      quote_identifiers=quote_identifiers)
             assert m_execute.called and any(map(lambda e: 'COPY INTO' in str(e.args), m_execute.call_args_list))
 
 
-def test_location_building_db_schema_quoted_identifiers(conn_cnx):
-    """This tests that write_pandas constructs location correctly with database, schema and table name."""
-    from snowflake.connector.cursor import SnowflakeCursor
-    with conn_cnx() as cnx:  # type: SnowflakeConnection
-        def mocked_execute(*args, **kwargs):
-            if len(args) >= 1 and args[0].startswith('COPY INTO'):
-                location = args[0].split(' ')[2]
-                assert location == '"database"."schema"."table"'
-            cur = SnowflakeCursor(cnx)
-            cur._result = iter([])
-            return cur
-        with mock.patch('snowflake.connector.cursor.SnowflakeCursor.execute', side_effect=mocked_execute) as m_execute:
-            success, nchunks, nrows, _ = write_pandas(cnx, sf_connector_version_df, "table",
-                                                      database='database', schema='schema', quote_identifiers=True)
-            assert m_execute.called and any(map(lambda e: 'COPY INTO' in str(e.args), m_execute.call_args_list))
-
-
-def test_location_building_schema(conn_cnx):
+@pytest.mark.parametrize('quote_identifiers', [True, False])
+def test_location_building_schema(conn_cnx, quote_identifiers: bool):
     """This tests that write_pandas constructs location correctly with schema and table name."""
     from snowflake.connector.cursor import SnowflakeCursor
     with conn_cnx() as cnx:  # type: SnowflakeConnection
         def mocked_execute(*args, **kwargs):
             if len(args) >= 1 and args[0].startswith('COPY INTO'):
                 location = args[0].split(' ')[2]
-                assert location == 'schema.table'
+                if quote_identifiers:
+                    assert location == '"schema"."table"'
+                else:
+                    assert location == 'schema.table'
             cur = SnowflakeCursor(cnx)
             cur._result = iter([])
             return cur
         with mock.patch('snowflake.connector.cursor.SnowflakeCursor.execute', side_effect=mocked_execute) as m_execute:
             success, nchunks, nrows, _ = write_pandas(cnx, sf_connector_version_df, "table",
-                                                      schema='schema')
+                                                      schema='schema', quote_identifiers=quote_identifiers)
             assert m_execute.called and any(map(lambda e: 'COPY INTO' in str(e.args), m_execute.call_args_list))
 
 
-def test_location_building_schema_quoted_identifier(conn_cnx):
+@pytest.mark.parametrize('quote_identifiers', [True, False])
+def test_location_building(conn_cnx, quote_identifiers: bool):
     """This tests that write_pandas constructs location correctly with schema and table name."""
     from snowflake.connector.cursor import SnowflakeCursor
     with conn_cnx() as cnx:  # type: SnowflakeConnection
         def mocked_execute(*args, **kwargs):
             if len(args) >= 1 and args[0].startswith('COPY INTO'):
                 location = args[0].split(' ')[2]
-                assert location == '"schema"."table"'
+                if quote_identifiers:
+                    assert location == '"teble.table"'
+                else:
+                    assert location == 'teble.table'
             cur = SnowflakeCursor(cnx)
             cur._result = iter([])
             return cur
         with mock.patch('snowflake.connector.cursor.SnowflakeCursor.execute', side_effect=mocked_execute) as m_execute:
-            success, nchunks, nrows, _ = write_pandas(cnx, sf_connector_version_df, "table",
-                                                      schema='schema', quote_identifiers=True)
-            assert m_execute.called and any(map(lambda e: 'COPY INTO' in str(e.args), m_execute.call_args_list))
-
-
-def test_location_building(conn_cnx):
-    """This tests that write_pandas constructs location correctly with schema and table name."""
-    from snowflake.connector.cursor import SnowflakeCursor
-    with conn_cnx() as cnx:  # type: SnowflakeConnection
-        def mocked_execute(*args, **kwargs):
-            if len(args) >= 1 and args[0].startswith('COPY INTO'):
-                location = args[0].split(' ')[2]
-                assert location == 'teble.table'
-            cur = SnowflakeCursor(cnx)
-            cur._result = iter([])
-            return cur
-        with mock.patch('snowflake.connector.cursor.SnowflakeCursor.execute', side_effect=mocked_execute) as m_execute:
-            success, nchunks, nrows, _ = write_pandas(cnx, sf_connector_version_df, "teble.table")
-            assert m_execute.called and any(map(lambda e: 'COPY INTO' in str(e.args), m_execute.call_args_list))
-
-
-def test_location_building_quoted_identifiers(conn_cnx):
-    """This tests that write_pandas constructs location correctly with schema and table name."""
-    from snowflake.connector.cursor import SnowflakeCursor
-    with conn_cnx() as cnx:  # type: SnowflakeConnection
-        def mocked_execute(*args, **kwargs):
-            if len(args) >= 1 and args[0].startswith('COPY INTO'):
-                location = args[0].split(' ')[2]
-                assert location == '"teble.table"'
-            cur = SnowflakeCursor(cnx)
-            cur._result = iter([])
-            return cur
-        with mock.patch('snowflake.connector.cursor.SnowflakeCursor.execute', side_effect=mocked_execute) as m_execute:
-            success, nchunks, nrows, _ = write_pandas(cnx, sf_connector_version_df, "teble.table", quote_identifiers=True)
+            success, nchunks, nrows, _ = write_pandas(cnx, sf_connector_version_df, "teble.table",
+                                                      quote_identifiers=quote_identifiers)
             assert m_execute.called and any(map(lambda e: 'COPY INTO' in str(e.args), m_execute.call_args_list))
