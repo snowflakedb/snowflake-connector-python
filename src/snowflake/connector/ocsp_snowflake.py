17aed29e37f802db551c296a1c5deaee6124e7b7
sfc-gh-hchaturvedi <harsh.chaturvedi@snowflake.com>
2020-10-08 17:27:29 +0000

    Added more error codes to the map and BugFix

--- src/snowflake/connector/ocsp_snowflake.py
+++ src/snowflake/connector/ocsp_snowflake.py
@@ -1184,7 +1184,7 @@ class SnowflakeOCSP(object):
                         # TODO - Remove this potentially broken OCSP Response / SSD
                         raise op_er
                 else:
-                    telemetry_data.set_event_sub_type(OCSPTelemetryData.ERROR_CODE_MAP(op_er.errno))
+                    telemetry_data.set_event_sub_type(OCSPTelemetryData.ERROR_CODE_MAP[op_er.errno])
                     raise op_er
 
         except RevocationCheckError as rce:
@@ -1221,7 +1221,7 @@ class SnowflakeOCSP(object):
         try:
             self._check_ocsp_response_cache_server(cert_data)
         except RevocationCheckError as rce:
-            telemetry_data.set_event_sub_type(OCSPTelemetryData.ERROR_CODE_MAP(rce.errno))
+            telemetry_data.set_event_sub_type(OCSPTelemetryData.ERROR_CODE_MAP[rce.errno])
         except Exception as ex:
             logger.debug("Caught unknown exception - %s. Continue to validate by direct connection", str(ex))
 
