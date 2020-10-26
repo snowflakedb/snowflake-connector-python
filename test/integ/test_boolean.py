7cb3193539180a02659bb3035d97687452e18567
Shige Takeda <shige.takeda@snowflake.com>
2020-10-26 22:15:23 +0000

    add one olddriver mark

--- test/integ/test_boolean.py
+++ test/integ/test_boolean.py
@@ -3,6 +3,9 @@
 #
 # Copyright (c) 2012-2020 Snowflake Computing Inc. All right reserved.
 #
+import pytest
+
+pytestmark = pytest.mark.olddriver
 
 
 def test_binding_fetching_boolean(conn_cnx, db_parameters):
