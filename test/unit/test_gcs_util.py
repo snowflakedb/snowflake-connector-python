beb986ad48442b03b96a3d15ea70150d402bb04c
Mark Keller <mark.keller@snowflake.com>
2020-10-27 05:42:46 +0000

    added more unit tests for gcs_util.py

--- test/unit/test_gcs_util.py
+++ test/unit/test_gcs_util.py
@@ -6,7 +6,15 @@
 
 import logging
 
+import mock
+import pytest
+import requests
+from requests import Response
+
+from snowflake.connector.constants import ResultStatus
 from snowflake.connector.gcs_util import SnowflakeGCSUtil
+
+from ..randomize import random_string
 
 
 def test_create_client(caplog):
@@ -17,3 +25,89 @@ def test_create_client(caplog):
         ('snowflake.connector.gcs_util', logging.DEBUG, "len(GCS_ACCESS_TOKEN): 10"),
         ('snowflake.connector.gcs_util', logging.DEBUG, "GCS operations with an access token are currently unsupported")
     ]
+
+
+def test_native_download_access_token(caplog):
+    """Tests that GCS access token error is correctly logged when downloading."""
+    meta = {}
+    SnowflakeGCSUtil._native_download_file(meta, None, 99)
+    assert meta['result_status'] == ResultStatus.ERROR
+    assert caplog.record_tuples == [
+        ('snowflake.connector.gcs_util', logging.ERROR, "GCS download operation with an access token is "
+                                                        "currently unsupported"),
+    ]
+
+
+def test_native_upload_access_token(caplog):
+    """Tests that GCS access token error is correctly logged when uploading."""
+    meta = {}
+    SnowflakeGCSUtil.upload_file(None, meta, None, 99)
+    assert meta['result_status'] == ResultStatus.ERROR
+    assert caplog.record_tuples == [
+        ('snowflake.connector.gcs_util', logging.ERROR, "GCS upload operation with an access token is "
+                                                        "currently unsupported"),
+    ]
+
+
+@pytest.mark.parametrize('errno', [403, 408, 429, 500, 503])
+def test_upload_retry_errors(errno, tmpdir):
+    """Tests whether retryable errors are handled correctly when upploading."""
+    f_name = str(tmpdir.join('some_file.txt'))
+    resp = Response()
+    resp.status_code = errno
+    meta = {'presigned_url': ['some_url'], 'sha256_digest': 'asd'}
+    with open(f_name, 'w') as f:
+        f.write(random_string(15))
+    with mock.patch('requests.put', side_effect=requests.exceptions.HTTPError(response=resp)):
+        SnowflakeGCSUtil.upload_file(f_name, meta, None, 99)
+        assert isinstance(meta['last_error'], requests.exceptions.HTTPError)
+        assert meta['result_status'] == ResultStatus.NEED_RETRY
+
+
+@pytest.mark.parametrize('errno', [403, 408, 429, 500, 503])
+def test_download_retry_errors(errno, tmpdir):
+    """Tests whether retryable errors are handled correctly when downloading."""
+    resp = Response()
+    resp.status_code = errno
+    meta = {'presigned_url': ['some_url'], 'sha256_digest': 'asd'}
+    with mock.patch('requests.get', side_effect=requests.exceptions.HTTPError(response=resp)):
+        SnowflakeGCSUtil._native_download_file(meta, str(tmpdir), 99)
+        assert isinstance(meta['last_error'], requests.exceptions.HTTPError)
+        assert meta['result_status'] == ResultStatus.NEED_RETRY
+
+
+def test_upload_put_timeout(tmpdir, caplog):
+    """Tests whether timeout error is handled correctly when uploading."""
+    f_name = str(tmpdir.join('some_file.txt'))
+    resp = Response()
+    meta = {'presigned_url': ['some_url'], 'sha256_digest': 'asd'}
+    with open(f_name, 'w') as f:
+        f.write(random_string(15))
+    with mock.patch('requests.put', side_effect=requests.exceptions.Timeout(response=resp)):
+        SnowflakeGCSUtil.upload_file(f_name, meta, None, 99)
+    assert isinstance(meta['last_error'], requests.exceptions.Timeout)
+    assert meta['result_status'] == ResultStatus.NEED_RETRY
+    assert caplog.record_tuples == [
+        ('snowflake.connector.gcs_util', logging.DEBUG, 'GCS file upload Timeout Error: ')
+    ]
+
+
+def test_upload_get_timeout(tmpdir, caplog):
+    """Tests whether timeout error is handled correctly when downloading."""
+    resp = Response()
+    meta = {'presigned_url': ['some_url'], 'sha256_digest': 'asd'}
+    with mock.patch('requests.get', side_effect=requests.exceptions.Timeout(response=resp)):
+        SnowflakeGCSUtil._native_download_file(meta, str(tmpdir), 99)
+    assert isinstance(meta['last_error'], requests.exceptions.Timeout)
+    assert meta['result_status'] == ResultStatus.NEED_RETRY
+    assert caplog.record_tuples == [
+        ('snowflake.connector.gcs_util', logging.DEBUG, 'GCS file download Timeout Error: ')
+    ]
+
+
+def test_get_file_header_none():
+    """Tests whether default file handle created by get_file_header is as expected."""
+    file_header = SnowflakeGCSUtil.get_file_header({}, 'file')
+    assert file_header.digest is None
+    assert file_header.content_length is None
+    assert file_header.encryption_metadata is None
