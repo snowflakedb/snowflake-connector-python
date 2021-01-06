4df2eb03791b7ec30aa800a4ea5a4b70eec62680
Mark Keller <63477823+sfc-gh-mkeller@users.noreply.github.com>
2021-01-06 03:04:47 +0000

    SNOW-256116 retiring flake8 tests (#590)

--- ci/build_windows.bat
+++ ci/build_windows.bat
@@ -19,10 +19,7 @@ if %errorlevel% neq 0 goto :error
 call %venv_dir%\scripts\activate
 if %errorlevel% neq 0 goto :error
 
-python -m pip install --upgrade pip setuptools flake8
-if %errorlevel% neq 0 goto :error
-
-flake8
+python -m pip install --upgrade pip setuptools wheel
 if %errorlevel% neq 0 goto :error
 
 (for %%v in (%python_versions%) do (
