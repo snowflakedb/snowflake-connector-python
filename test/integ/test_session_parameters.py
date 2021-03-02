475ab34b4a99a9ecf452435e6e23a9f2fcc430da
sfc-gh-hchaturvedi <harsh.chaturvedi@snowflake.com>
2021-03-02 02:37:47 +0000

    Update tests

--- test/integ/test_session_parameters.py
+++ test/integ/test_session_parameters.py
@@ -34,8 +34,17 @@ def test_client_session_keep_alive(db_parameters):
     session parameter is always honored and given higher precedence over
     user and account level backend configuration.
     """
+    admin_cnxn = snowflake.connector.connect(
+        protocol=db_parameters['sf_protocol'],
+        account=db_parameters['sf_account'],
+        user=db_parameters['sf_user'],
+        password=db_parameters['sf_password'],
+        host=db_parameters['sf_host'],
+        port=db_parameters['sf_port']
+    )
+
     # Ensure backend parameter is set to False
-    set_backend_client_session_keep_alive(db_parameters, False)
+    set_backend_client_session_keep_alive(db_parameters, admin_cnxn, False)
     connection = create_client_connection(db_parameters, True)
 
     ret = connection.cursor().execute(
@@ -44,7 +53,7 @@ def test_client_session_keep_alive(db_parameters):
     connection.close()
 
     # Set backend parameter to True
-    set_backend_client_session_keep_alive(db_parameters, True)
+    set_backend_client_session_keep_alive(db_parameters, admin_cnxn, True)
 
     # Set session parameter to False
     connection = create_client_connection(db_parameters, False)
@@ -77,20 +86,12 @@ def create_client_connection(db_parameters: object, val: bool) -> object:
     return connection
 
 
-def set_backend_client_session_keep_alive(db_parameters: object, val: bool) -> None:
+def set_backend_client_session_keep_alive(db_parameters: object, admin_cnx: object, val: bool) -> None:
     """Set both at Account level and User level."""
-    connection = snowflake.connector.connect(
-        protocol=db_parameters['sf_protocol'],
-        account=db_parameters['sf_account'],
-        user=db_parameters['sf_user'],
-        password=db_parameters['sf_password'],
-        host=db_parameters['sf_host'],
-        port=db_parameters['sf_port']
-    )
     query = "alter account {} set CLIENT_SESSION_KEEP_ALIVE={}".format(
         db_parameters['account'], str(val))
-    connection.cursor().execute(query)
+    admin_cnx.cursor().execute(query)
 
     query = "alter user {}.{} set CLIENT_SESSION_KEEP_ALIVE={}".format(
                     db_parameters['account'], db_parameters['user'], str(val))
-    connection.cursor().execute(query)
+    admin_cnx.cursor().execute(query)
