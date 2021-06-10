ebc0687133b74a8650e802a275630abd2ee882d4
Mark Keller <mark.keller@snowflake.com>
2021-06-10 02:21:08 +0000

    add logic to switch between old and new code-path

--- src/snowflake/connector/feature.py
+++ src/snowflake/connector/feature.py
@@ -3,7 +3,17 @@
 #
 # Copyright (c) 2012-2021 Snowflake Computing Inc. All right reserved.
 #
+import os
 
 # Feature flags
 
 feature_use_pyopenssl = True  # use pyopenssl API or openssl command
+
+
+# use the new code-path without using SDK to put and get files
+def feature_sdkless_put() -> bool:
+    return os.environ.get("SF_SDKLESS_PUT", "").lower() == "true"
+
+
+def feature_sdkless_get() -> bool:
+    return os.environ.get("SF_SDKLESS_GET", "").lower() == "true"
