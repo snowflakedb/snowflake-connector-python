e46d160c22b4121d9a598368fe34890cbd81cfda
Kushan Zaveri <kushan.zaveri@snowflake.com>
2021-06-17 17:01:37 +0000

    Revert "Revert "SNOW-296993 reworking internals for result_set and result_chunks (#688)""
    This reverts commit c5c31faadb8989792b4452a4fb551a99dbdd57c5.

---
+++ test/unit/conftest.py
@@ -0,0 +1,16 @@
+#
+# Copyright (c) 2012-2021 Snowflake Computing Inc. All right reserved.
+#
+import pytest
+
+from snowflake.connector.telemetry_oob import TelemetryService
+
+
+@pytest.fixture(autouse=True, scope="session")
+def disable_oob_telemetry():
+    oob_telemetry_service = TelemetryService.get_instance()
+    original_state = oob_telemetry_service.enabled
+    oob_telemetry_service.disable()
+    yield None
+    if original_state:
+        oob_telemetry_service.enable()
