eb545fbc624af481ca1a6294b4a6e13d7ae008bb
Mark Keller <mark.keller@snowflake.com>
2021-06-30 23:25:16 +0000

    WIP: more unit tests rewrites/fixes

--- test/unit/test_gcs_client.py
+++ test/unit/test_gcs_client.py
@@ -5,7 +5,6 @@
 #
 
 import logging
-import os
 from unittest.mock import Mock
 
 import mock
@@ -13,6 +12,7 @@ import pytest
 
 from snowflake.connector import SnowflakeConnection
 from snowflake.connector.constants import ResultStatus
+from snowflake.connector.vendored.requests import Response
 
 from ..randomize import random_string
 
@@ -20,6 +20,7 @@ try:
     from snowflake.connector.errors import RequestExceedMaxRetryError
     from snowflake.connector.file_transfer_agent import (
         SnowflakeFileMeta,
+        SnowflakeFileTransferAgent,
         StorageCredential,
     )
     from snowflake.connector.storage_client import METHODS
@@ -46,21 +47,6 @@ except ImportError:  # pragma: no cover
     import requests
 
     vendored_request = False
-
-
-@pytest.fixture(
-    autouse=True,
-    params=[pytest.param(True, marks=pytest.mark.skipolddriver), False],
-    ids=["sdkless", "sdkfull"],
-)
-def sdkless(request):
-    if request.param:
-        os.environ["SF_SDKLESS_PUT"] = "true"
-        os.environ["SF_SDKLESS_GET"] = "true"
-    else:
-        os.environ["SF_SDKLESS_PUT"] = "false"
-        os.environ["SF_SDKLESS_GET"] = "false"
-    return request.param
 
 
 @pytest.mark.parametrize("errno", [408, 429, 500, 503])
@@ -129,7 +115,7 @@ def test_upload_retry_errors(errno, tmpdir, sdkless):
             assert meta.result_status == ResultStatus.NEED_RETRY
 
 
-def test_upload_uncaught_exception(tmpdir):
+def test_upload_uncaught_exception(tmpdir, sdkless):
     """Tests whether non-retryable errors are handled correctly when uploading."""
     f_name = str(tmpdir.join("some_file.txt"))
     resp = requests.Response()
@@ -141,20 +127,49 @@ def test_upload_uncaught_exception(tmpdir):
         presigned_url="some_url",
         sha256_digest="asd",
     )
+    exc = requests.exceptions.HTTPError(response=resp)
     with open(f_name, "w") as f:
         f.write(random_string(15))
-    with mock.patch(
-        "snowflake.connector.vendored.requests.put"
-        if vendored_request
-        else "requests.put",
-        side_effect=requests.exceptions.HTTPError(response=resp),
-    ):
-        with pytest.raises(requests.exceptions.HTTPError):
-            SnowflakeGCSRestClient.upload_file(f_name, meta, None, 99, 64000)
+    if sdkless:
+        agent = SnowflakeFileTransferAgent(
+            mock.MagicMock(),
+            f"put {f_name} @~",
+            {
+                "data": {
+                    "command": "UPLOAD",
+                    "src_locations": [f_name],
+                    "stageInfo": {
+                        "locationType": "GCS",
+                        "location": "",
+                        "creds": {"AWS_SECRET_KEY": "", "AWS_KEY_ID": ""},
+                        "region": "test",
+                        "endPoint": None,
+                    },
+                    "localLocation": "/tmp",
+                }
+            },
+        )
+        with mock.patch(
+            "snowflake.connector.gcs_storage_client.SnowflakeGCSRestClient.get_file_header",
+        ), mock.patch(
+            "snowflake.connector.gcs_storage_client.SnowflakeGCSRestClient._upload_chunk",
+            side_effect=exc,
+        ):
+            agent.execute()
+        assert agent._file_metadata[0].error_details is exc
+    else:
+        with mock.patch(
+            "snowflake.connector.vendored.requests.put"
+            if vendored_request
+            else "requests.put",
+            side_effect=exc,
+        ):
+            with pytest.raises(requests.exceptions.HTTPError):
+                SnowflakeGCSUtil.upload_file(f_name, meta, None, 99, 64000)
 
 
 @pytest.mark.parametrize("errno", [403, 408, 429, 500, 503])
-def test_download_retry_errors(errno, tmp_path):
+def test_download_retry_errors(errno, tmp_path, sdkless):
     """Tests whether retryable errors are handled correctly when downloading."""
     resp = requests.Response()
     resp.status_code = errno
@@ -165,18 +180,21 @@ def test_download_retry_errors(errno, tmp_path):
         presigned_url="some_url",
         sha256_digest="asd",
     )
-    with mock.patch(
-        "snowflake.connector.vendored.requests.get"
-        if vendored_request
-        else "requests.get",
-        side_effect=requests.exceptions.HTTPError(response=resp),
-    ):
-        SnowflakeGCSRestClient._native_download_file(meta, str(tmp_path), 99)
-        assert isinstance(meta.last_error, requests.exceptions.HTTPError)
-        assert meta.result_status == ResultStatus.NEED_RETRY
+    if sdkless:
+        assert False  # TODO
+    else:
+        with mock.patch(
+            "snowflake.connector.vendored.requests.get"
+            if vendored_request
+            else "requests.get",
+            side_effect=requests.exceptions.HTTPError(response=resp),
+        ):
+            SnowflakeGCSUtil._native_download_file(meta, str(tmp_path), 99)
+            assert isinstance(meta.last_error, requests.exceptions.HTTPError)
+            assert meta.result_status == ResultStatus.NEED_RETRY
 
 
-def test_download_uncaught_exception(tmp_path):
+def test_download_uncaught_exception(tmp_path, sdkless):
     """Tests whether non-retryable errors are handled correctly when downloading."""
     resp = requests.Response()
     resp.status_code = 501
@@ -197,7 +215,7 @@ def test_download_uncaught_exception(tmp_path):
             SnowflakeGCSRestClient._native_download_file(meta, str(tmp_path), 99)
 
 
-def test_upload_put_timeout(tmp_path, caplog):
+def test_upload_put_timeout(tmp_path, caplog, sdkless):
     """Tests whether timeout error is handled correctly when uploading."""
     caplog.set_level(logging.DEBUG, "snowflake.connector")
     f_name = str(tmp_path / "some_file.txt")
@@ -211,30 +229,70 @@ def test_upload_put_timeout(tmp_path, caplog):
     )
     with open(f_name, "w") as f:
         f.write(random_string(15))
-    with mock.patch(
-        "snowflake.connector.vendored.requests.put"
-        if vendored_request
-        else "requests.put",
-        side_effect=requests.exceptions.Timeout(response=resp),
-    ):
-        SnowflakeGCSRestClient.upload_file(f_name, meta, None, 99, 64000)
-    assert isinstance(meta.last_error, requests.exceptions.Timeout)
-    assert meta.result_status == ResultStatus.NEED_RETRY
-    assert all(
-        [
-            log in caplog.record_tuples
-            for log in [
-                (
-                    "snowflake.connector.gcs_util",
-                    logging.DEBUG,
-                    "GCS file upload Timeout Error: ",
-                )
+    if sdkless:
+        agent = SnowflakeFileTransferAgent(
+            mock.MagicMock(),
+            f"put {f_name} @~",
+            {
+                "data": {
+                    "command": "UPLOAD",
+                    "src_locations": [f_name],
+                    "stageInfo": {
+                        "locationType": "GCS",
+                        "location": "",
+                        "creds": {"AWS_SECRET_KEY": "", "AWS_KEY_ID": ""},
+                        "region": "test",
+                        "endPoint": None,
+                    },
+                    "localLocation": "/tmp",
+                }
+            },
+        )
+        mocked_put, mocked_head = mock.MagicMock(), mock.MagicMock()
+        mocked_put.side_effect = requests.exceptions.Timeout(response=resp)
+        resp = Response()
+        resp.status_code = 404
+        mocked_head.return_value = resp
+        SnowflakeGCSRestClient.SLEEP_UNIT = 0
+        from snowflake.connector.storage_client import METHODS
+
+        with mock.patch.dict(METHODS, {"PUT": mocked_put, "HEAD": mocked_head}):
+            agent.execute()
+        assert (
+            "snowflake.connector.storage_client",
+            logging.WARNING,
+            "PUT with url https://storage.googleapis.com//some_file.txt.gz failed for transient error: ",
+        ) in caplog.record_tuples
+        assert (
+            "snowflake.connector.file_transfer_agent",
+            logging.DEBUG,
+            "Chunk 0 of file some_file.txt failed to transfer for unexpected exception PUT with url https://storage.googleapis.com//some_file.txt.gz failed for exceeding maximum retries.",
+        ) in caplog.record_tuples
+    else:
+        with mock.patch(
+            "snowflake.connector.vendored.requests.put"
+            if vendored_request
+            else "requests.put",
+            side_effect=requests.exceptions.Timeout(response=resp),
+        ):
+            SnowflakeGCSUtil.upload_file(f_name, meta, None, 99, 64000)
+        assert isinstance(meta.last_error, requests.exceptions.Timeout)
+        assert meta.result_status == ResultStatus.NEED_RETRY
+        assert all(
+            [
+                log in caplog.record_tuples
+                for log in [
+                    (
+                        "snowflake.connector.gcs_util_sdk",
+                        logging.DEBUG,
+                        "GCS file upload Timeout Error: ",
+                    )
+                ]
             ]
-        ]
-    )
+        )
 
 
-def test_upload_get_timeout(tmp_path, caplog):
+def test_upload_get_timeout(tmp_path, caplog, sdkless):
     """Tests whether timeout error is handled correctly when downloading."""
     caplog.set_level(logging.DEBUG, "snowflake.connector")
     resp = requests.Response()
@@ -261,7 +319,7 @@ def test_upload_get_timeout(tmp_path, caplog):
     ) in caplog.record_tuples
 
 
-def test_get_file_header_none_with_presigned_url(tmp_path):
+def test_get_file_header_none_with_presigned_url(tmp_path, sdkless):
     """Tests whether default file handle created by get_file_header is as expected."""
     meta = SnowflakeFileMeta(
         name=str(tmp_path / "some_file"),
