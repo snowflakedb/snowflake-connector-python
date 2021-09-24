ae073149198ccf8f8cda7bc1b5255595a9e5980a
Mark Keller <63477823+sfc-gh-mkeller@users.noreply.github.com>
2021-09-24 13:44:25 -0700

    SNOW-466099 fixed remote file HEAD bug (#875)

--- src/snowflake/connector/storage_client.py
+++ src/snowflake/connector/storage_client.py
@@ -319,7 +319,7 @@ class SnowflakeStorageClient(ABC):
             os.makedirs(base_dir)
 
         # HEAD
-        file_header = self.get_file_header(self.meta.dst_file_name)
+        file_header = self.get_file_header(self.meta.real_src_file_name)
 
         if file_header and file_header.encryption_metadata:
             self.encryption_metadata = file_header.encryption_metadata
