ada8e1aad71afa2abfb6118fe28d768905e8818b
Mark Keller <mark.keller@snowflake.com>
2021-11-15 18:23:59 +0000

    adding delocate to our Jenkins build

--- ci/build_darwin.sh
+++ ci/build_darwin.sh
@@ -34,9 +34,10 @@ for PYTHON_VERSION in ${PYTHON_VERSIONS}; do
     # Clean up possible build artifacts
     rm -rf build generated_version.py
     # Update PEP-517 dependencies
-    python -m pip install -U pip setuptools wheel build
+    python -m pip install -U pip setuptools wheel build delocate
     # Use new PEP-517 build
-    python -m build --wheel .
+    python -m build --wheel --outdir dist_tmp .
+    delocate-wheel --wheel-dir dist dist_tmp/*.whl
     deactivate
     echo "[Info] Deleting venv at ${VENV_DIR}"
     rm -rf ${VENV_DIR}
