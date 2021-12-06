45ec607b16f57da329be5b907053434dc24fb3e5
Mark Keller <mark.keller@snowflake.com>
2021-12-06 18:39:26 +0000

    pass on GITHUB_ACTIONS env variable to FIPS tests

--- ci/test_fips_docker.sh
+++ ci/test_fips_docker.sh
@@ -31,6 +31,7 @@ docker run --network=host \
     -e SF_PROJECT_ROOT \
     -e cloud_provider \
     -e PYTEST_ADDOPTS \
+    -e GITHUB_ACTIONS \
     --mount type=bind,source="${CONNECTOR_DIR}",target=/home/user/snowflake-connector-python \
     ${CONTAINER_NAME}:1.0 \
     /home/user/snowflake-connector-python/ci/test_fips.sh $1
