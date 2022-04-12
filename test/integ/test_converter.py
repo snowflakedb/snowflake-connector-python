44862bffb1fd20286cd86f7d77ca30d525b27d47
sfc-gh-jbahk <63611797+sfc-gh-jbahk@users.noreply.github.com>
2022-04-11 22:18:39 -0400

    SNOW-499493 Add Interval Precision to Timestamp (#1030)
    Co-authored-by: Mark Keller <mark.keller@snowflake.com>

--- test/integ/test_converter.py
+++ test/integ/test_converter.py
@@ -352,7 +352,7 @@ def test_fetch_timestamps_negative_epoch(conn_cnx):
     with conn_cnx() as cnx:
         cur = cnx.cursor()
         cur.execute(
-            """
+            """\
 SELECT
     '1950-11-27 12:34:56.123456'::timestamp_ntz(6),
     '2012-01-03 12:34:56.123456'::timestamp_ntz(6)
