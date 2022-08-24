88938c6382f8b125697de2d2518bb2b0da7bea8e
Adam Ling <adam.ling@snowflake.com>
2022-08-24 10:22:16 -0700

    fix oob tests

--- test/unit/test_telemetry_oob.py
+++ test/unit/test_telemetry_oob.py
@@ -7,12 +7,12 @@ from __future__ import annotations
 import pytest
 
 import snowflake.connector.errorcode
+import snowflake.connector.telemetry
 from snowflake.connector.description import CLIENT_NAME, SNOWFLAKE_CONNECTOR_VERSION
 from snowflake.connector.errorcode import ER_FAILED_TO_REQUEST
 from snowflake.connector.errors import RevocationCheckError
 from snowflake.connector.ocsp_snowflake import OCSPTelemetryData
 from snowflake.connector.sqlstate import SQLSTATE_CONNECTION_WAS_NOT_ESTABLISHED
-from snowflake.connector.telemetry import TelemetryField, generate_telemetry_data
 from snowflake.connector.telemetry_oob import TelemetryService
 
 DEV_CONFIG = {
@@ -156,33 +156,37 @@ def test_telemetry_oob_http_log_urgent(telemetry_setup):
 
 
 def test_generate_telemetry_with_driver_info():
-    assert generate_telemetry_data(is_oob_telemetry=True) == {
-        TelemetryField.KEY_OOB_DRIVER.value: CLIENT_NAME,
-        TelemetryField.KEY_OOB_VERSION.value: SNOWFLAKE_CONNECTOR_VERSION,
+    assert snowflake.connector.telemetry.generate_telemetry_data(
+        is_oob_telemetry=True
+    ) == {
+        snowflake.connector.telemetry.TelemetryField.KEY_OOB_DRIVER.value: CLIENT_NAME,
+        snowflake.connector.telemetry.TelemetryField.KEY_OOB_VERSION.value: SNOWFLAKE_CONNECTOR_VERSION,
     }
 
-    assert generate_telemetry_data(from_dict={}, is_oob_telemetry=True) == {
-        TelemetryField.KEY_OOB_DRIVER.value: CLIENT_NAME,
-        TelemetryField.KEY_OOB_VERSION.value: SNOWFLAKE_CONNECTOR_VERSION,
+    assert snowflake.connector.telemetry.generate_telemetry_data(
+        from_dict={}, is_oob_telemetry=True
+    ) == {
+        snowflake.connector.telemetry.TelemetryField.KEY_OOB_DRIVER.value: CLIENT_NAME,
+        snowflake.connector.telemetry.TelemetryField.KEY_OOB_VERSION.value: SNOWFLAKE_CONNECTOR_VERSION,
     }
 
-    assert generate_telemetry_data(
+    assert snowflake.connector.telemetry.generate_telemetry_data(
         from_dict={"key": "value"}, is_oob_telemetry=True
     ) == {
-        TelemetryField.KEY_OOB_DRIVER.value: CLIENT_NAME,
-        TelemetryField.KEY_OOB_VERSION.value: SNOWFLAKE_CONNECTOR_VERSION,
+        snowflake.connector.telemetry.TelemetryField.KEY_OOB_DRIVER.value: CLIENT_NAME,
+        snowflake.connector.telemetry.TelemetryField.KEY_OOB_VERSION.value: SNOWFLAKE_CONNECTOR_VERSION,
         "key": "value",
     }
 
-    assert generate_telemetry_data(
+    assert snowflake.connector.telemetry.generate_telemetry_data(
         from_dict={
-            TelemetryField.KEY_OOB_DRIVER.value: "CUSTOM_CLIENT_NAME",
-            TelemetryField.KEY_OOB_VERSION.value: "1.2.3",
+            snowflake.connector.telemetry.TelemetryField.KEY_OOB_DRIVER.value: "CUSTOM_CLIENT_NAME",
+            snowflake.connector.telemetry.TelemetryField.KEY_OOB_VERSION.value: "1.2.3",
             "key": "value",
         },
         is_oob_telemetry=True,
     ) == {
-        TelemetryField.KEY_OOB_DRIVER.value: "CUSTOM_CLIENT_NAME",
-        TelemetryField.KEY_OOB_VERSION.value: "1.2.3",
+        snowflake.connector.telemetry.TelemetryField.KEY_OOB_DRIVER.value: "CUSTOM_CLIENT_NAME",
+        snowflake.connector.telemetry.TelemetryField.KEY_OOB_VERSION.value: "1.2.3",
         "key": "value",
     }
