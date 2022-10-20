08e41c3f9459dc289b93f4567ab46b059af0f32f
sfc-gh-sfan <shixuan.fan@snowflake.com>
2022-10-20 16:36:06 -0700

    SNOW-680774 Check CLIENT_LOG_DIR_PATH_DOCKER for ENABLE_TELEMETRY_LOG
    Description
    
    test_util is supposed to only be used by snowflake regression test,
    but currently it runs for all Jenkins linux hosts, which is not
    desired. A quick fix is to also check CLIENT_LOG_DIR_PATH_DOCKER,
    which presumbly is not used by most customers if not all.
    
    Testing
    
    existing tests

--- src/snowflake/connector/test_util.py
+++ src/snowflake/connector/test_util.py
@@ -11,12 +11,13 @@ import os
 from .compat import IS_LINUX
 
 RUNNING_ON_JENKINS = os.getenv("JENKINS_HOME") is not None
-REGRESSION_TEST_LOG_DIR = os.getenv("CLIENT_LOG_DIR_PATH_DOCKER", "/tmp")
-ENABLE_TELEMETRY_LOG = RUNNING_ON_JENKINS and IS_LINUX
+HAS_REGRESSION_TEST_LOG_DIR = os.getenv("CLIENT_LOG_DIR_PATH_DOCKER") is not None
+ENABLE_TELEMETRY_LOG = RUNNING_ON_JENKINS and HAS_REGRESSION_TEST_LOG_DIR and IS_LINUX
 rt_plain_logger = None
 
 
 if ENABLE_TELEMETRY_LOG:
+    REGRESSION_TEST_LOG_DIR = os.getenv("CLIENT_LOG_DIR_PATH_DOCKER")
     rt_plain_logger = logging.getLogger("regression.test.plain.logger")
     rt_plain_logger.setLevel(logging.DEBUG)
     ch = logging.FileHandler(
