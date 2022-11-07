0b2f96c8d841f7e0e8186156ea69193bbd6d7062
Adam Ling <adam.ling@snowflake.com>
2022-11-07 13:00:45 -0800

    test older connector

--- ci/test_fips.sh
+++ ci/test_fips.sh
@@ -11,13 +11,14 @@ CONNECTOR_WHL="$(ls $CONNECTOR_DIR/dist/*cp38*manylinux2014*.whl | sort -r | hea
 python3.8 -m venv fips_env
 source fips_env/bin/activate
 pip install -U setuptools pip
-pip install "${CONNECTOR_WHL}[pandas,secure-local-storage,development]" "cryptography<3.3.0" --force-reinstall --no-binary cryptography
+pip install "snowflake-connector-python[pandas,secure-local-storage,development]==2.7.9" "cryptography<3.3.0" --force-reinstall --no-binary cryptography
 
 echo "!!! Environment description !!!"
 echo "Default installed OpenSSL version"
 openssl version
 python -c "import ssl; print('Python openssl library: ' + ssl.OPENSSL_VERSION)"
 python -c  "from cryptography.hazmat.backends.openssl import backend;print('Cryptography openssl library: ' + backend.openssl_version_text())"
+python -c  "from cryptography.hazmat.backends import default_backend;print(default_backend())"
 pip freeze
 
 cd $CONNECTOR_DIR
