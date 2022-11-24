9e04925e159f90462e7860bf44627244b326a0d9
Kris Wagner <32375658+sfc-gh-kwagner@users.noreply.github.com>
2022-11-23 19:17:10 -0800

    SNOW-630142 Custom Auth (#1215)
    Co-authored-by: Adam Ling <adam.ling@snowflake.com>
    Co-authored-by: Mark Keller <mark.keller@snowflake.com>

--- test/unit/test_auth_oauth.py
+++ test/unit/test_auth_oauth.py
@@ -5,14 +5,16 @@
 
 from __future__ import annotations
 
-from snowflake.connector.auth_oauth import AuthByOAuth
+try:  # pragma: no cover
+    from snowflake.connector.auth import AuthByOAuth
+except ImportError:
+    from snowflake.connector.auth_oauth import AuthByOAuth
 
 
 def test_auth_oauth():
     """Simple OAuth test."""
     token = "oAuthToken"
     auth = AuthByOAuth(token)
-    auth.authenticate(None, None, None, None, None)
     body = {"data": {}}
     auth.update_body(body)
     assert body["data"]["TOKEN"] == token, body
