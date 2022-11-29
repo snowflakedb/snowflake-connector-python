73b1a66d042960b8dbcb289532e979dfb5c9c1b5
Mark Keller <mark.keller@snowflake.com>
2022-11-28 16:19:21 -0800

    Revert "Revert "SNOW-630142 Custom Auth (#1215)""
    This reverts commit 871424ef839a8ab39dfabc05ca19e14fda673b35.

--- src/snowflake/connector/network.py
+++ src/snowflake/connector/network.py
@@ -15,7 +15,7 @@ import time
 import traceback
 import uuid
 from threading import Lock
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Any
 
 import OpenSSL.SSL
 
@@ -671,7 +671,7 @@ class SnowflakeRestful:
             except ReauthenticationRequest as ex:
                 if self._connection._authenticator != EXTERNAL_BROWSER_AUTHENTICATOR:
                     raise ex.cause
-                ret = self._connection._reauthenticate_by_webbrowser()
+                ret = self._connection._reauthenticate()
             logger.debug(
                 "ret[code] = {code} after renew_session".format(
                     code=(ret.get("code", "N/A"))
@@ -724,7 +724,7 @@ class SnowflakeRestful:
             except ReauthenticationRequest as ex:
                 if self._connection._authenticator != EXTERNAL_BROWSER_AUTHENTICATOR:
                     raise ex.cause
-                ret = self._connection._reauthenticate_by_webbrowser()
+                ret = self._connection._reauthenticate()
             logger.debug(
                 "ret[code] = {code} after renew_session".format(
                     code=(ret.get("code", "N/A"))
@@ -756,7 +756,15 @@ class SnowflakeRestful:
 
         return ret
 
-    def fetch(self, method, full_url, headers, data=None, timeout=None, **kwargs):
+    def fetch(
+        self,
+        method: str,
+        full_url: str,
+        headers: dict[str, Any],
+        data: dict[str, Any] | None = None,
+        timeout: int | None = None,
+        **kwargs,
+    ) -> dict[Any, Any]:
         """Carry out API request with session management."""
 
         class RetryCtx:
