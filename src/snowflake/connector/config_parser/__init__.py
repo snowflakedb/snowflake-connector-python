115dc7e7cfc6e757d7124eca954901b1d103b599
Mark Keller <mark.keller@snowflake.com>
2023-05-18 20:01:10 -0700

    removing default config file creation

--- src/snowflake/connector/config_parser/__init__.py
+++ src/snowflake/connector/config_parser/__init__.py
@@ -4,19 +4,10 @@
 
 from __future__ import annotations
 
-from pathlib import Path
-from shutil import copyfile
-
 from tomlkit import parse
 
 from ..constants import config_file
 from .parser import ConfigParser
-
-# Create default configuration file, if it doesn't exist
-if not config_file.exists():
-    # Create default config file
-    default_config = Path(__file__).absolute().parent / "default_config.toml"
-    copyfile(default_config, config_file)
 
 CONFIG_PARSER = ConfigParser(
     name="CONFIG_PARSER",
