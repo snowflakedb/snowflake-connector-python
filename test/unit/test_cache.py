95fbe580adf273fdd441b941813c1f36ef2aeffe
Adam Ling <adam.ling@snowflake.com>
2023-07-11 15:45:59 -0700

    update test

--- test/unit/test_cache.py
+++ test/unit/test_cache.py
@@ -489,14 +489,14 @@ class TestSFDictFileCache:
 def test_file_is_not_updated(tmpdir):
     tmp_cache_file = os.path.join(tmpdir, "tmp_cache")
     sfcache = cache.SFDictFileCache(file_path=tmp_cache_file, entry_lifetime=1)
-    sfcache["key"] = "value"
+    sfcache.update({"key": "value"})
     assert sfcache._cache_modified
     sfcache._save()  # this save call will dump cache to file because item was added
     assert not sfcache._cache_modified
     updated_time = os.path.getmtime(tmp_cache_file)
     sfcache._save()  # this save call will be a no-op since there is no update
     assert os.path.getmtime(tmp_cache_file) == updated_time
-    sfcache["key"] = "value2"
+    sfcache.update({"key": "value2"})
     assert sfcache._cache_modified
     time.sleep(0.1)  # sleep 0.1 to avoid flushing too fast
     sfcache._save()  # this save call will dump cache to file
