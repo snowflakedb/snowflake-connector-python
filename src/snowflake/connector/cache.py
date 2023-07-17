b768295f4cd7bc9b4a27e706b3d697a2ee44d8e5
Mark Keller <mark.keller@snowflake.com>
2023-07-17 13:32:51 -0700

    Update src/snowflake/connector/cache.py
    Co-authored-by: Adam Ling <adam.ling@snowflake.com>

--- src/snowflake/connector/cache.py
+++ src/snowflake/connector/cache.py
@@ -236,8 +236,7 @@ class SFDictCache(Generic[K, V]):
                 g = iter(other.items())
             to_insert = {k: CacheEntry(expiry=expiry, entry=v) for k, v in g}
         elif isinstance(other, SFDictCache):
-            with other._lock:
-                other._clear_expired_entries()
+            other.clear_expired_entries()
             others_items = list(other._cache.items())
             # Only accept values from another cache if their key is not in self,
             #  or if expiry is later the self known one
