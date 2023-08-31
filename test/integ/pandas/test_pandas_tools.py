521361cbdb3a6ee43a9bb5b41b31766f010cd4eb
dvorst <87502756+dvorst@users.noreply.github.com>
2023-08-31 21:58:27 +0200

    STYLE rearranged imports test_pandas_tools.py

--- test/integ/pandas/test_pandas_tools.py
+++ test/integ/pandas/test_pandas_tools.py
@@ -6,7 +6,7 @@
 from __future__ import annotations
 
 import math
-from datetime import datetime, timezone, timedelta
+from datetime import datetime, timedelta, timezone
 from typing import TYPE_CHECKING, Any, Callable, Generator
 from unittest import mock
 
