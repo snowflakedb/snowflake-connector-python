a8a92f133109504ed83c018b92b429d191522478
Adam Ling <adam.ling@snowflake.com>
2023-10-17 09:56:45 -0700

    skipolddriver

--- test/unit/test_compute_chunk_size.py
+++ test/unit/test_compute_chunk_size.py
@@ -4,22 +4,28 @@
 
 import pytest
 
-from snowflake.connector.compute_chunk_size import ChunkSizeCalculator, constants
+try:
+    from snowflake.connector.compute_chunk_size import ChunkSizeCalculator, constants
 
-chunk_size_calculator = ChunkSizeCalculator()
-expected_chunk_size = constants["CURRENT_CHUNK_SIZE"]
-max_part_size = constants["MAX_PART_SIZE"]
-min_part_size = constants["MIN_PART_SIZE"]
-max_object_size = constants["MAX_OBJECT_SIZE"]
-sample_file_size_2gb = 2 * 1024 * 1024 * 1024
-sample_file_size_85gb = 85 * 1024 * 1024 * 1024
-sample_file_size_5tb = 4.9 * 1024 * 1024 * 1024 * 1024
-sample_file_size_6tb = 6 * 1024 * 1024 * 1024 * 1024
-sample_chunk_size_4mb = 4 * 1024 * 1024
-sample_chunk_size_10mb = 10 * 1024 * 1024
+    expected_chunk_size = constants["CURRENT_CHUNK_SIZE"]
+    max_part_size = constants["MAX_PART_SIZE"]
+    min_part_size = constants["MIN_PART_SIZE"]
+    max_object_size = constants["MAX_OBJECT_SIZE"]
+    sample_file_size_2gb = 2 * 1024 * 1024 * 1024
+    sample_file_size_85gb = 85 * 1024 * 1024 * 1024
+    sample_file_size_5tb = 4.9 * 1024 * 1024 * 1024 * 1024
+    sample_file_size_6tb = 6 * 1024 * 1024 * 1024 * 1024
+    sample_chunk_size_4mb = 4 * 1024 * 1024
+    sample_chunk_size_10mb = 10 * 1024 * 1024
+except ImportError:
+    pass
+
+
+pytestmark = pytest.mark.skipolddriver
 
 
 def test_check_chunk_size():
+    chunk_size_calculator = ChunkSizeCalculator()
     chunk_size_1 = chunk_size_calculator.compute_chunk_size(sample_file_size_2gb)
     assert chunk_size_1 == expected_chunk_size
     chunk_size_2 = chunk_size_calculator.compute_chunk_size(sample_file_size_5tb)
@@ -33,6 +39,7 @@ def test_check_chunk_size():
 
 
 def test_check_min_chunk_size():
+    chunk_size_calculator = ChunkSizeCalculator()
     chunk_size_1 = chunk_size_calculator._check_min_chunk_size(sample_chunk_size_4mb)
     assert chunk_size_1 == min_part_size
 
@@ -41,6 +48,7 @@ def test_check_min_chunk_size():
 
 
 def test_check_max_parts():
+    chunk_size_calculator = ChunkSizeCalculator()
     chunk_size_3 = chunk_size_calculator._check_max_parts(
         expected_chunk_size, sample_file_size_85gb
     )
