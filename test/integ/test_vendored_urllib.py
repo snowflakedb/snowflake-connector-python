7358f8d55bd1d5d265b28f3dee3d5f0b3f170bce
Afroz Alam <afroz.alam@snowflake.com>
2023-10-26 15:36:31 -0700

    Fix urllib bug in vendored library

---
+++ test/integ/test_vendored_urllib.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+#
+# Copyright (c) 2012-2023 Snowflake Computing Inc. All rights reserved.
+#
+
+from snowflake.connector.vendored import urllib3
+
+
+def test_local_fix_for_closed_socket_bug():
+    # https://github.com/urllib3/urllib3/issues/1878#issuecomment-641534573
+    http = urllib3.PoolManager(maxsize=1)
+
+    def _execute_request():
+        resp = http.request(
+            method="GET", url="http://httpbin.org", preload_content=False
+        )
+        resp._connection.sock.close()
+        resp.release_conn()
+        resp.close()
+        return resp
+
+    _execute_request()
+    try:
+        _execute_request()
+    except ValueError as e:
+        if "file descriptor cannot be a negative" in str(e):
+            raise AssertionError(
+                "Second _execute_request failed. See linked github issue comment"
+            )
+        else:
+            raise e
