9b996a8c2aa1c4f416f01136285cee3e30533c69
Patryk Czajka <patryk.czajka@snowflake.com>
2024-01-19 11:49:56 +0100

    More readable permission warning message

--- src/snowflake/connector/config_manager.py
+++ src/snowflake/connector/config_manager.py
@@ -336,7 +336,7 @@ class ConfigManager:
             ):
                 # for non-Windows, suggest change to 0600 permissions.
                 chmod_message = (
-                    f". To change owner, run `chown $USER {str(filep)}`. To restrict permissions, run `chmod 0600 {str(filep)}`."
+                    f'.\n * To change owner, run `chown $USER "{str(filep)}"`.\n * To restrict permissions, run `chmod 0600 "{str(filep)}"`.\n'
                     if not IS_WINDOWS
                     else ""
                 )
