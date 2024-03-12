add0451cfc12f991dabae01235471d668af78b75
Chris Conner <chris.conner@snowflake.com>
2024-03-12 10:34:06 -0400

    Fix windows tests

--- test/unit/test_connection_diagnostic.py
+++ test/unit/test_connection_diagnostic.py
@@ -97,21 +97,21 @@ def test_report_message_if_bad_allowlist(caplog):
         )
         tmp_file.flush()
 
-        try:
-            snowflake.connector.connect(
-                account="testaccount",
-                user="testuser",
-                password="testpassword",
-                database="TESTDB",
-                warehouse="TESTWH",
-                enable_connection_diag=True,
-                connection_diag_allowlist_path=tmp_file.name,
-            )
-        except Exception:
-            pass
+    try:
+        snowflake.connector.connect(
+            account="testaccount",
+            user="testuser",
+            password="testpassword",
+            database="TESTDB",
+            warehouse="TESTWH",
+            enable_connection_diag=True,
+            connection_diag_allowlist_path=tmp_file.name,
+        )
+    except Exception:
+        pass
 
-        os.unlink(tmp_file.name)
-        assert "Allowlist is not a valid list of json objects" in caplog.text
+    os.unlink(tmp_file.name)
+    assert "Allowlist is not a valid list of json objects" in caplog.text
 
 
 def test_validate_allowlist_file_works(caplog):
@@ -120,21 +120,21 @@ def test_validate_allowlist_file_works(caplog):
         tmp_file.write('[{"host":"s3.amazonaws.com","port":443,"type":"STAGE"}]')
         tmp_file.flush()
 
-        try:
-            snowflake.connector.connect(
-                account="testaccount",
-                user="testuser",
-                password="testpassword",
-                database="TESTDB",
-                warehouse="TESTWH",
-                enable_connection_diag=True,
-                connection_diag_allowlist_path=tmp_file.name,
-            )
-        except Exception:
-            pass
+    try:
+        snowflake.connector.connect(
+            account="testaccount",
+            user="testuser",
+            password="testpassword",
+            database="TESTDB",
+            warehouse="TESTWH",
+            enable_connection_diag=True,
+            connection_diag_allowlist_path=tmp_file.name,
+        )
+    except Exception:
+        pass
 
-        os.unlink(tmp_file.name)
-        assert "STAGE: s3.amazonaws.com" in caplog.text
+    os.unlink(tmp_file.name)
+    assert "STAGE: s3.amazonaws.com" in caplog.text
 
 
 def test_validate_whitelist_file_works(caplog):
@@ -143,18 +143,18 @@ def test_validate_whitelist_file_works(caplog):
         tmp_file.write('[{"host":"s3.amazonaws.com","port":443,"type":"STAGE"}]')
         tmp_file.flush()
 
-        try:
-            snowflake.connector.connect(
-                account="testaccount",
-                user="testuser",
-                password="testpassword",
-                database="TESTDB",
-                warehouse="TESTWH",
-                enable_connection_diag=True,
-                connection_diag_whitelist_path=tmp_file.name,
-            )
-        except Exception:
-            pass
+    try:
+        snowflake.connector.connect(
+            account="testaccount",
+            user="testuser",
+            password="testpassword",
+            database="TESTDB",
+            warehouse="TESTWH",
+            enable_connection_diag=True,
+            connection_diag_whitelist_path=tmp_file.name,
+        )
+    except Exception:
+        pass
 
-        os.unlink(tmp_file.name)
-        assert "STAGE: s3.amazonaws.com" in caplog.text
+    os.unlink(tmp_file.name)
+    assert "STAGE: s3.amazonaws.com" in caplog.text
