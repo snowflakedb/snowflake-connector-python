3b0f66f6f9c97a22ffda0b78aea11d99f7723b79
Yuyang Wang <yuyang.wang@snowflake.com>
2024-04-15 10:37:45 -0700

    add none type check

--- src/snowflake/connector/s3_storage_client.py
+++ src/snowflake/connector/s3_storage_client.py
@@ -94,7 +94,7 @@ class SnowflakeS3RestClient(SnowflakeStorageClient):
         self, use_accelerate_endpoint: bool | None = None
     ) -> bool:
         # accelerate cannot be used in China and us government
-        if "S3China" in self.location_type:
+        if self.location_type and "S3China" in self.location_type:
             return False
         # if self.endpoint has been set, e.g. by metadata, no more config is needed.
         if self.endpoint is not None:
