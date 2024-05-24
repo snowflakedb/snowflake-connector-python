7328e2d8b8bd23ea0e056dc886d2247745eff92c
Adam Ling <adam.ling@snowflake.com>
2024-05-23 18:42:00 -0700

    debug raise

--- test/integ/test_put_get_user_stage.py
+++ test/integ/test_put_get_user_stage.py
@@ -98,9 +98,10 @@ def test_put_get_accelerate_user_stage(tmpdir, conn_cnx, from_path, accelerate_c
             )
             config_accl = mocked_file_agent.agent._use_accelerate_endpoint
             if accelerate_config:
-                assert (config_accl is True) and all(
+                if not ((config_accl is True) and all(
                     ele.find("s3-acc") >= 0 for ele in endpoints
-                )
+                )):
+                    raise ValueError(f'can not find s3-acc endpoin {str(endpoints)}')
             else:
                 assert (config_accl is False) and all(
                     ele.find("s3-acc") < 0 for ele in endpoints
