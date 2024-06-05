3c683243046876770da9ffcb56646dd3ab01fc2b
Adam Ling <adam.ling@snowflake.com>
2024-06-05 16:38:18 -0700

    comment

--- src/snowflake/connector/ocsp_snowflake.py
+++ src/snowflake/connector/ocsp_snowflake.py
@@ -441,7 +441,7 @@ class OCSPServer:
         # from urllib import parse
         # b64data = parse.quote_plus(b64data)
 
-        # is the change safe? this seems related to how the responder implements the logic of handling url
+        # will the change of parse.quote_plus(b64data) safe? this seems related to how the responder implements the logic of handling url
         # the concern here is if quote_plus would break existing cases
         # however, there is another chance that this path always does not work and our test does not cover it.
         if self.OCSP_RETRY_URL is None:
@@ -1427,15 +1427,10 @@ class SnowflakeOCSP:
                 actual_method = "get"
 
             if actual_method == "get":
-                # the logic here feels weird, `actual_method` is `post`, but we are
-                # generating a "get" url and set payload to None which feels more like this is a "get" op?
-                # shouldn't the get and post be flipped?
                 b64data = self.decode_ocsp_request_b64(ocsp_request)
                 target_url = self.OCSP_CACHE_SERVER.generate_get_url(ocsp_url, b64data)
                 payload = None
             else:
-                # would enforcing "get" rather than "post" work?
-                # can try it by flipping the flag 'use_post_method' in ssl_wrap_socket
                 target_url = ocsp_url
                 payload = self.decode_ocsp_request(ocsp_request)
                 headers["Content-Type"] = "application/ocsp-request"
