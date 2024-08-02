614bafad4ec24df609e3a1cb59857b3315d7fb55
Adam Ling <adam.ling@snowflake.com>
2024-08-01 20:39:12 -0700

    add test file

---
+++ test/integ/aio/test_connection_async.py
@@ -0,0 +1,29 @@
+#
+# Copyright (c) 2012-2023 Snowflake Computing Inc. All rights reserved.
+#
+
+import pytest
+
+from snowflake.connector.aio.connection_async import SnowflakeConnectionAsync
+
+pytestmark = pytest.mark.asyncio
+
+
+async def test_basic(db_parameters):
+    """Basic Connection test without schema."""
+    cnx = SnowflakeConnectionAsync(
+        user=db_parameters["user"],
+        password=db_parameters["password"],
+        host=db_parameters["host"],
+        port=db_parameters["port"],
+        account=db_parameters["account"],
+        database=db_parameters["database"],
+        protocol=db_parameters["protocol"],
+        timezone="UTC",
+    )
+    await cnx.connect()
+    cursor = cnx.cursor()
+    await cursor.execute("select 1")
+    assert await cursor.fetchone() == (1,)
+    assert cnx, "invalid cnx"
+    await cnx.close()
