a483ec9a1338342bef22ca04af6a78c8956f370c
Adam Ling <adam.ling@snowflake.com>
2024-08-08 11:08:24 -0700

    SNOW-1617614: asyncio network implementation, test set up (#2019)

---
+++ src/snowflake/connector/aio/__init__.py
@@ -0,0 +1,9 @@
+#
+# Copyright (c) 2012-2023 Snowflake Computing Inc. All rights reserved.
+#
+
+from __future__ import annotations
+
+from ._connection import SnowflakeConnection
+
+__all__ = [SnowflakeConnection]
