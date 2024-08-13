266e71d67b47f0563d177111a7de3ed0b1acdc70
Yuyang Wang <yuyang.wang@snowflake.com>
2024-08-13 13:14:33 -0700

    save

--- src/snowflake/connector/aio/__init__.py
+++ src/snowflake/connector/aio/__init__.py
@@ -4,6 +4,16 @@
 
 from __future__ import annotations
 
+from functools import wraps
+
 from ._connection import SnowflakeConnection
 
 __all__ = [SnowflakeConnection]
+
+
+@wraps(SnowflakeConnection.__init__)
+def Connect(**kwargs) -> SnowflakeConnection:
+    return SnowflakeConnection(**kwargs)
+
+
+connect = Connect
