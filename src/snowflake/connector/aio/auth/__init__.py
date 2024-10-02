853abd77f83f829c62868a58060e60a07b50ff6a
Adam Ling <adam.ling@snowflake.com>
2024-10-02 14:48:02 -0700

    impl auth

--- src/snowflake/connector/aio/auth/__init__.py
+++ src/snowflake/connector/aio/auth/__init__.py
@@ -4,12 +4,38 @@
 
 from __future__ import annotations
 
+from ...auth.by_plugin import AuthType
 from ._auth import Auth
 from ._by_plugin import AuthByPlugin
 from ._default import AuthByDefault
+from ._idtoken import AuthByIdToken
+from ._keypair import AuthByKeyPair
+from ._oauth import AuthByOAuth
+from ._okta import AuthByOkta
+from ._usrpwdmfa import AuthByUsrPwdMfa
+from ._webbrowser import AuthByWebBrowser
+
+FIRST_PARTY_AUTHENTICATORS = frozenset(
+    (
+        AuthByDefault,
+        AuthByKeyPair,
+        AuthByOAuth,
+        AuthByOkta,
+        AuthByUsrPwdMfa,
+        AuthByWebBrowser,
+        AuthByIdToken,
+    )
+)
 
 __all__ = [
-    AuthByDefault,
-    Auth,
-    AuthByPlugin,
+    "AuthByPlugin",
+    "AuthByDefault",
+    "AuthByKeyPair",
+    "AuthByOAuth",
+    "AuthByOkta",
+    "AuthByUsrPwdMfa",
+    "AuthByWebBrowser",
+    "Auth",
+    "AuthType",
+    "FIRST_PARTY_AUTHENTICATORS",
 ]
