d0eae907f3d0f554caa453d02a2f0f5a89c68ec9
Adam Ling <adam.ling@snowflake.com>
2024-10-17 10:28:10 -0700

    add tests

--- test/unit/test_backoff_policies.py
+++ test/unit/test_backoff_policies.py
@@ -13,7 +13,11 @@ except ImportError:
 @pytest.mark.skipolddriver
 def test_linear_backoff():
     backoff_generator = linear_backoff(factor=2, base=1, cap=100, enable_jitter=False)()
-    assert [next(backoff_generator) for _ in range(5)] == [1, 3, 5, 7, 9]
+    # after 50 calls, we will reach the max 100 and won't grow
+    # 1, 3, 5, 7, 9, ... 100, 100, 100, 100, 100, max is the cap 100
+    assert [next(backoff_generator) for _ in range(60)] == [
+        i for i in range(1, 100, 2)
+    ] + [100] * 10
 
 
 @pytest.mark.skipolddriver
@@ -21,4 +25,16 @@ def test_exponential_backoff():
     backoff_generator = exponential_backoff(
         factor=2, base=1, cap=100, enable_jitter=False
     )()
-    assert [next(backoff_generator) for _ in range(5)] == [1, 2, 4, 8, 16]
+    # after 7 calls, we will reach max which is the cap 100 and won't grow
+    assert [next(backoff_generator) for _ in range(10)] == [
+        1,
+        2,
+        4,
+        8,
+        16,
+        32,
+        64,
+        100,
+        100,
+        100,
+    ]
