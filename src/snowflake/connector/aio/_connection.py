a0572901fae0bbbf67cb5cdbdee284e4b82aa47b
Yuyang Wang <yuyang.wang@snowflake.com>
2024-10-23 09:52:40 -0700

    fix import issue

--- src/snowflake/connector/aio/_connection.py
+++ src/snowflake/connector/aio/_connection.py
@@ -65,12 +65,13 @@ from ..network import (
     ReauthenticationRequest,
 )
 from ..sqlstate import SQLSTATE_CONNECTION_NOT_EXISTS, SQLSTATE_FEATURE_NOT_SUPPORTED
+from ..telemetry import TelemetryField
 from ..time_util import get_time_millis
 from ..util_text import split_statements
 from ._cursor import SnowflakeCursor
 from ._description import CLIENT_NAME
 from ._network import SnowflakeRestful
-from ._telemetry import TelemetryClient, TelemetryData, TelemetryField
+from ._telemetry import TelemetryClient, TelemetryData
 from ._time_util import HeartBeatTimer
 from .auth import (
     FIRST_PARTY_AUTHENTICATORS,
