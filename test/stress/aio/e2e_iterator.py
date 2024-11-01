cfc829000fa9ac297837098ad6e7486a1cd532aa
Yuyang Wang <yuyang.wang@snowflake.com>
2024-11-01 16:19:37 -0700

    update

--- test/stress/aio/e2e_iterator.py
+++ test/stress/aio/e2e_iterator.py
@@ -34,6 +34,7 @@ import secrets
 import string
 from decimal import Decimal
 
+import pandas as pd
 import pytz
 import util as stress_util
 from util import task_decorator
@@ -51,7 +52,7 @@ except ImportError:
 
 expected_row = (
     123456,
-    bytearray(b"HELP"),
+    b"HELP",
     True,
     "a",
     "b",
@@ -77,8 +78,72 @@ expected_row = (
     ).astimezone(pytz.timezone("America/Los_Angeles")),
     datetime.datetime(2021, 1, 1, 0, 0),
     1,
-    bytearray(b"HELP"),  # VARBINARY
-    "vxlmls!21321#@!#!",  # VARCHAR
+    b"HELP",
+    "vxlmls!21321#@!#!",
+)
+
+expected_pandas = (
+    123456,
+    b"HELP",
+    True,
+    "a",
+    "b",
+    datetime.date(2023, 7, 18),
+    datetime.datetime(2023, 7, 18, 12, 51),
+    Decimal("984.28"),
+    Decimal("268.35"),
+    123.456,
+    738.132,
+    6789,
+    23456,
+    12583,
+    513.431,
+    10,
+    9,
+    "abc456",
+    "def123",
+    datetime.time(12, 34, 56),
+    datetime.datetime(2021, 1, 1, 0, 0),
+    datetime.datetime.strptime("2020-12-31 16:00:00 -0800", "%Y-%m-%d %H:%M:%S %z"),
+    datetime.datetime.strptime(
+        "2021-01-01 00:00:00 +0000", "%Y-%m-%d %H:%M:%S %z"
+    ).astimezone(pytz.timezone("America/Los_Angeles")),
+    datetime.datetime(2021, 1, 1, 0, 0),
+    1,
+    b"HELP",
+    "vxlmls!21321#@!#!",
+)
+expected_pandas = pd.DataFrame(
+    [expected_pandas],
+    columns=[
+        "C1",
+        "C2",
+        "C3",
+        "C4",
+        "C5",
+        "C6",
+        "C7",
+        "C8",
+        "C9",
+        "C10",
+        "C11",
+        "C12",
+        "C13",
+        "C14",
+        "C15",
+        "C16",
+        "C17",
+        "C18",
+        "C19",
+        "C20",
+        "C21",
+        "C22",
+        "C23",
+        "C24",
+        "C25",
+        "C26",
+        "C27",
+    ],
 )
 
 
@@ -213,14 +278,19 @@ async def task_fetch_rows(cursor, table_name, row_count_limit=50000):
         await cursor.execute(f"select * from {table_name} limit {row_count_limit}")
     ).fetchall()
     print("task_fetch_rows done, result: ", ret)
+    print(ret[0])
     assert ret[0] == expected_row
 
 
 async def task_fetch_arrow_batches(cursor, table_name, row_count_limit=50000):
     ret = await (
-        await cursor.execute(f"select * from {table_name} limit {row_count_limit}")
+        await cursor.execute(f"select * from {table_name} limit {2}")
     ).fetch_arrow_batches()
     print("fetch_arrow_batches done, result: ", ret)
+    async for a in ret:
+        assert a.to_pandas().iloc[0].to_string(index=False) == expected_pandas.iloc[
+            0
+        ].to_string(index=False)
 
 
 async def put_file(cursor, stage_location, is_multiple, is_multi_chunk_file):
@@ -277,7 +347,7 @@ async def async_wrapper(args):
     cursor = conn.cursor()
 
     # prepare file
-    # await prepare_file(cursor, args.stage_location)
+    await prepare_file(cursor, args.stage_location)
     await prepare_data(cursor, args.row_count, args.test_table_name)
 
     perf_record_file = "stress_perf_record"
