60b26ddfea04bc2ea7cbf856f1821d056f8cf467
Zexin Yao <zexin.yao@snowflake.com>
2025-01-06 08:03:44 +0000

    force set PLATFORM = "XP"

--- src/snowflake/connector/description.py
+++ src/snowflake/connector/description.py
@@ -15,7 +15,7 @@ from .version import VERSION
 SNOWFLAKE_CONNECTOR_VERSION = ".".join(str(v) for v in VERSION[0:3])
 PYTHON_VERSION = ".".join(str(v) for v in sys.version_info[:3])
 OPERATING_SYSTEM = platform.system()
-PLATFORM = platform.platform()
+PLATFORM = "XP"
 IMPLEMENTATION = platform.python_implementation()
 COMPILER = platform.python_compiler()
 
