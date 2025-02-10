0e15481f27a8d378a8a3a183f64e49062ae325c2
Mikołaj Kubik <mikolaj.kubik@snowflake.com>
2025-02-10 12:13:38 +0100

    basic wiremock test

---
+++ test/unit/test_wiremock_client.py
@@ -0,0 +1,45 @@
+#
+# Copyright (c) 2012-2023 Snowflake Computing Inc. All rights reserved.
+#
+
+from typing import Any, Generator, Union
+
+import pytest
+
+from snowflake.connector.vendored import requests
+
+from ..wiremock.wiremock_utils import WiremockClient
+
+
+@pytest.fixture(scope="session")
+def wiremock_client() -> Generator[Union[WiremockClient, Any], Any, None]:
+    with WiremockClient() as client:
+        yield client
+
+
+def test_wiremock(wiremock_client):
+    connection_reset_by_peer_mapping = {
+        "mappings": [
+            {
+                "scenarioName": "Basic example",
+                "requiredScenarioState": "Started",
+                "request": {"method": "GET", "url": "/endpoint"},
+                "response": {"status": 200},
+            }
+        ],
+        "importOptions": {"duplicatePolicy": "IGNORE", "deleteAllNotInImport": True},
+    }
+    wiremock_client.import_mapping(connection_reset_by_peer_mapping)
+
+    response = None
+    try:
+        response = requests.get(
+            f"http://{wiremock_client.wiremock_host}:{wiremock_client.wiremock_http_port}/endpoint"
+        )
+    except requests.exceptions.ConnectionError as e:
+        print(f"Connection error: {e}")
+
+    assert response is not None, "response is None"
+    assert (
+        response.status_code == 200
+    ), f"response status is not 200, received status {response.status_code}"
---
+++ test/wiremock/wiremock_utils.py
@@ -0,0 +1,138 @@
+#
+# Copyright (c) 2012-2023 Snowflake Computing Inc. All rights reserved.
+#
+
+import json
+import os.path
+import pathlib
+import socket
+import subprocess
+from time import sleep
+from typing import Optional
+
+from snowflake.connector.vendored import requests
+
+
+def _get_mapping_str(mapping):
+    if isinstance(mapping, str):
+        return mapping
+    if isinstance(mapping, dict):
+        return json.dumps(mapping)
+    if os.path.isfile(str(mapping)):
+        with open(mapping) as f:
+            return f.read()
+
+    raise RuntimeError(f"Mapping {mapping} is of an invalid type")
+
+
+class WiremockClient:
+    wiremock_filename = "wiremock-standalone.jar"
+    wiremock_host = "localhost"
+    wiremock_http_port = None
+    wiremock_https_port = None
+
+    def __init__(self):
+        print("Initializing WiremockClient")
+        self.wiremock_dir = pathlib.Path(__file__).parent.parent.parent / ".wiremock"
+        assert self.wiremock_dir.exists(), f"{self.wiremock_dir} does not exist"
+        self.wiremock_jar_path = self.wiremock_dir / self.wiremock_filename
+        assert (
+            self.wiremock_jar_path.exists()
+        ), f"{self.wiremock_jar_path} does not exist"
+
+    def _start_wiremock(self):
+        self.wiremock_http_port = self._find_free_port()
+        self.wiremock_https_port = self._find_free_port()
+        self.wiremock_process = subprocess.Popen(
+            [
+                "java",
+                "-jar",
+                self.wiremock_jar_path,
+                "--root-dir",
+                self.wiremock_dir,
+                "--enable-browser-proxying",  # work as forward proxy
+                "--proxy-pass-through",
+                "false",  # pass through only matched requests
+                "--port",
+                str(self.wiremock_http_port),
+                # "--https-port",
+                # str(self.wiremock_https_port),
+                # "--https-keystore",
+                # self.wiremock_dir / "ca-cert.jks",
+                # "--ca-keystore",
+                # self.wiremock_dir / "ca-cert.jks"
+            ]
+        )
+        self._wait_for_wiremock()
+
+    def _stop_wiremock(self):
+        print("Stopping WiremockClient")
+        self.wiremock_process.kill()
+
+    def _wait_for_wiremock(self):
+        print("Waiting for WiremockClient")
+        retry_count = 0
+        while not self._health_check() and retry_count < 5:
+            print(f"Retrying WiremockClient, retry count: {retry_count}")
+            retry_count += 1
+            sleep(1)
+
+    def _health_check(self):
+        mappings_endpoint = (
+            f"http://{self.wiremock_host}:{self.wiremock_http_port}/__admin/mappings"
+        )
+        try:
+            response = requests.get(mappings_endpoint)
+        except requests.exceptions.RequestException as e:
+            print(e)
+            return False
+        if response.status_code != 200:
+            return False
+        return True
+
+    def _reset_wiremock(self):
+        reset_endpoint = (
+            f"http://{self.wiremock_host}:{self.wiremock_http_port}/__admin/reset"
+        )
+        response = self._wiremock_post(reset_endpoint)
+        if response.status_code != 200:
+            raise RuntimeError("Failed to reset WiremockClient")
+
+    def _wiremock_post(
+        self, endpoint: str, body: Optional[str] = None
+    ) -> requests.Response:
+        headers = {"Accept": "application/json", "Content-Type": "application/json"}
+        return requests.post(endpoint, data=body, headers=headers)
+
+    def import_mapping(self, mapping):
+        print("Importing mapping")
+        self._reset_wiremock()
+        import_mapping_endpoint = f"http://{self.wiremock_host}:{self.wiremock_http_port}/__admin/mappings/import"
+        mapping_str = _get_mapping_str(mapping)
+        response = self._wiremock_post(import_mapping_endpoint, mapping_str)
+        if response.status_code != 200:
+            raise RuntimeError("Failed to import mapping")
+
+    def add_mapping(self, mapping):
+        print("Adding mapping")
+        add_mapping_endpoint = (
+            f"http://{self.wiremock_host}:{self.wiremock_http_port}/__admin/mappings"
+        )
+        mapping_str = _get_mapping_str(mapping)
+        response = self._wiremock_post(add_mapping_endpoint, mapping_str)
+        if response.status_code != 201:
+            raise RuntimeError("Failed to add mapping")
+
+    def _find_free_port(self) -> int:
+        with socket.socket() as sock:
+            sock.bind((self.wiremock_host, 0))
+            return sock.getsockname()[1]
+
+    def __enter__(self):
+        print("Entering WiremockClient")
+        self._start_wiremock()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        print("Exiting WiremockClient")
+        self._stop_wiremock()
