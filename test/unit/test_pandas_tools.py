ccaf75fb85a7229ee0cafd8e06eba9a9df648d11
Grant Culp <culpgrant21@gmail.com>
2025-02-24 21:52:51 -0600

    update test_pandas_tools with precommit changes

--- test/unit/test_pandas_tools.py
+++ test/unit/test_pandas_tools.py
@@ -1,22 +1,35 @@
-from unittest.mock import MagicMock
+#
+# Copyright (c) 2012-2023 Snowflake Computing Inc. All rights reserved.
+#
+
 from typing import Union
+from unittest.mock import MagicMock
 
 import pandas as pd
 import pytest
 
-from .mock_utils import mock_connection
 from snowflake.connector import pandas_tools
+
+from .mock_utils import mock_connection
 
 
 @pytest.mark.parametrize(
     ("use_vectorized_scanner", "expected_file_format"),
     [
         (None, "FILE_FORMAT=(TYPE=PARQUET COMPRESSION=auto)"),
-        (True, "FILE_FORMAT=(TYPE=PARQUET COMPRESSION=auto USE_VECTORIZED_SCANNER=TRUE)"),
-        (False, "FILE_FORMAT=(TYPE=PARQUET COMPRESSION=auto USE_VECTORIZED_SCANNER=FALSE)"),
+        (
+            True,
+            "FILE_FORMAT=(TYPE=PARQUET COMPRESSION=auto USE_VECTORIZED_SCANNER=TRUE)",
+        ),
+        (
+            False,
+            "FILE_FORMAT=(TYPE=PARQUET COMPRESSION=auto USE_VECTORIZED_SCANNER=FALSE)",
+        ),
     ],
 )
-def test_write_pandas_use_vectorized_scanner(use_vectorized_scanner: Union[bool, None], expected_file_format: str):
+def test_write_pandas_use_vectorized_scanner(
+    use_vectorized_scanner: Union[bool, None], expected_file_format: str
+):
     # Setup Mocks
     df = pd.DataFrame({"col1": [1, 2, 3]})
 
@@ -34,12 +47,11 @@ def test_write_pandas_use_vectorized_scanner(use_vectorized_scanner: Union[bool,
         use_vectorized_scanner=use_vectorized_scanner,
     )
 
-    executed_sql_statements = [call[0][0] for call in mock_cursor.execute.call_args_list]
+    executed_sql_statements = [
+        call[0][0] for call in mock_cursor.execute.call_args_list
+    ]
 
     assert any(
-        'COPY INTO' in sql and expected_file_format in sql
+        "COPY INTO" in sql and expected_file_format in sql
         for sql in executed_sql_statements
     )
-
-
-
