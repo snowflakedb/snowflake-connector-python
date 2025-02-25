54bee3bfab8f977a0be74e9396729c8c59b73870
Mikołaj Kubik <mikolaj.kubik@snowflake.com>
2025-02-25 10:16:25 +0100

    re-enable flaky annotation on test put load from user stage

--- test/integ/test_put_get.py
+++ test/integ/test_put_get.py
@@ -237,7 +237,7 @@ put file://{test_data.test_data_dir}/ExecPlatform/Database/data/orders_10*.csv @
             cur.execute("drop table pytest_putget_t1")
 
 
-# @pytest.mark.flaky(reruns=3)
+@pytest.mark.flaky(reruns=3)
 @pytest.mark.skipif(
     not CONNECTION_PARAMETERS_ADMIN, reason="Snowflake admin account is not accessible."
 )
