95aa256034698b7a482e93e8c162e2997167ca5a
Mikołaj Kubik <mikolaj.kubik@snowflake.com>
2025-03-05 15:32:20 +0100

    verify if yum works on bumped manylinux

--- ci/set_base_image.sh
+++ ci/set_base_image.sh
@@ -8,7 +8,7 @@ if [[ -n "$NEXUS_PASSWORD" ]]; then
     echo "[INFO] Pull docker images from $INTERNAL_REPO"
     NEXUS_USER=${USERNAME:-jenkins}
     docker login --username "$NEXUS_USER" --password "$NEXUS_PASSWORD" $INTERNAL_REPO
-    export BASE_IMAGE_MANYLINUX2014=nexus.int.snowflakecomputing.com:8086/docker/manylinux2014_x86_64
+    export BASE_IMAGE_MANYLINUX2014=nexus.int.snowflakecomputing.com:8086/docker/manylinux2014_x86_64:test_mkubik
     export BASE_IMAGE_MANYLINUX2014AARCH64=nexus.int.snowflakecomputing.com:8086/docker/manylinux2014_aarch64
 else
     echo "[INFO] Pull docker images from public registry"
