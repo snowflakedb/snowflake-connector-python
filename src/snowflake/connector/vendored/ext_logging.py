41e7cf23c4a42fb4ecc5e48c53ba39e902039e65
Maxim Mishchenko <maxim.mishchenko@snowflake.com>
2025-03-11 14:50:42 +0100

    SNOW-1857047 inject extended networking debug logging

---
+++ src/snowflake/connector/vendored/ext_logging.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+#
+# Copyright (c) 2012-2023 Snowflake Computing Inc. All rights reserved.
+#
+
+from __future__ import annotations
+
+import logging
+import sys
+
+
+log = logging.getLogger(__name__)
+
+
+def _relevant_path(path: str) -> str | None:
+    pos = path.rfind("snowflake-connector-python/src/snowflake/connector/")
+    if pos != -1:
+        return path[pos + 51 :]
+    return None
+
+
+def _trace(frame, event, arg):
+    if event == "call":
+        rel_path = _relevant_path(frame.f_back.f_code.co_filename)
+        if rel_path:
+            log.debug(
+                f"EXT-NTWRK-LOG:%s:%d:%s",
+                rel_path,
+                frame.f_back.f_lineno,
+                frame.f_code.co_qualname,
+            )
+
+
+def enable_extended_networking_logging():
+    log.debug("Enable EXT-NTWRK-LOG")
+    sys.settrace(_trace)
+
+
+def disable_extended_networking_logging():
+    log.debug("Disable EXT-NTWRK-LOG")
+    sys.settrace(None)
