03ef978571710ed76757c8032f661b33ecdc54fd
Patryk Cyrek <patryk.cyrek@snowflake.com>
2025-03-20 10:09:55 +0100

    adding soteria scope

--- test/auth/test_key_pair.py
+++ test/auth/test_key_pair.py
@@ -1,19 +1,43 @@
-from test.auth.authorization_parameters import AuthConnectionParameters, get_rsa_private_key_for_key_pair
+#
+# Copyright (c) 2012-2023 Snowflake Computing Inc. All rights reserved.
+#
+
+from test.auth.authorization_parameters import (
+    AuthConnectionParameters,
+    get_rsa_private_key_for_key_pair,
+)
 from test.auth.authorization_test_helper import AuthorizationTestHelper
 
+import pytest
 
+
+@pytest.mark.auth_test
 def test_key_pair_successful():
-    connection_parameters = AuthConnectionParameters().get_key_pair_connection_parameters()
-    connection_parameters["private_key"] = get_rsa_private_key_for_key_pair("SNOWFLAKE_AUTH_TEST_PRIVATE_KEY_PATH")
+    connection_parameters = (
+        AuthConnectionParameters().get_key_pair_connection_parameters()
+    )
+    connection_parameters["private_key"] = get_rsa_private_key_for_key_pair(
+        "SNOWFLAKE_AUTH_TEST_PRIVATE_KEY_PATH"
+    )
 
     test_helper = AuthorizationTestHelper(connection_parameters)
-    assert test_helper.connect_and_execute_simple_query(), "Failed to connect with Snowflake"
+    assert (
+        test_helper.connect_and_execute_simple_query()
+    ), "Failed to connect with Snowflake"
     assert test_helper.error_msg == "", "Error message should be empty"
 
+
+@pytest.mark.auth_test
 def test_key_pair_invalid_key():
-    connection_parameters = AuthConnectionParameters().get_key_pair_connection_parameters()
-    connection_parameters["private_key"] = get_rsa_private_key_for_key_pair("SNOWFLAKE_AUTH_TEST_INVALID_PRIVATE_KEY_PATH")
+    connection_parameters = (
+        AuthConnectionParameters().get_key_pair_connection_parameters()
+    )
+    connection_parameters["private_key"] = get_rsa_private_key_for_key_pair(
+        "SNOWFLAKE_AUTH_TEST_INVALID_PRIVATE_KEY_PATH"
+    )
 
     test_helper = AuthorizationTestHelper(connection_parameters)
-    assert not test_helper.connect_and_execute_simple_query(), "Connection to Snowflake should not be established"
+    assert (
+        not test_helper.connect_and_execute_simple_query()
+    ), "Connection to Snowflake should not be established"
     assert "JWT token is invalid" in test_helper.get_error_msg()
