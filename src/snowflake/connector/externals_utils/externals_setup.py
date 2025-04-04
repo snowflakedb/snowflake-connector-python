c3266c01a8618ee25cb88f3dfa96fed60647295f
Filip Pawłowski <filip.pawlowski@snowflake.com>
2025-04-04 15:25:16 +0200

    SNOW-2011595: Detailed module path specified

--- src/snowflake/connector/externals_utils/externals_setup.py
+++ src/snowflake/connector/externals_utils/externals_setup.py
@@ -4,7 +4,9 @@ from logging import getLogger
 
 from ..secret_detector import SecretDetector
 
-URLLIB3_MODULE_NAME = "snowflake.connector.vendored.urllib3"
+URLLIB3_MODULES_NAMES = ["snowflake.connector.vendored.urllib3.connectionpool"]
+EXTERNAL_LOGS_FORMAT = "%(asctime)s %(levelname)s %(message)s"
+
 # TODO: after migration to the external urllib3 from the vendored one, we should change filters here immediately to the below module's logger:
 # URLLIB3_MODULE_NAME = "urllib3"
 
@@ -13,8 +15,9 @@ def add_formatters_to_urllib3_loggers():
     # Import to make sure the logger of urllib3 was set up already and handlers were added
     import snowflake.connector.vendored.urllib3  # noqa: F401
 
-    for handler in getLogger(URLLIB3_MODULE_NAME).handlers:
-        handler.setFormatter(SecretDetector("%(asctime)s %(levelname)s %(message)s"))
+    for module_name in URLLIB3_MODULES_NAMES:
+        for handler in getLogger(module_name).handlers:
+            handler.setFormatter(SecretDetector(EXTERNAL_LOGS_FORMAT))
 
 
 def setup_external_libraries():
