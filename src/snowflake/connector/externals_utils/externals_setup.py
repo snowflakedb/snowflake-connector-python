eb4b8cb9aa4c681782a4efc9b0c6a10b5502f547
Filip Pawłowski <filip.pawlowski@snowflake.com>
2025-04-04 14:05:25 +0200

    SNOW-2011595: External libraries setup added.

--- src/snowflake/connector/externals_utils/externals_setup.py
+++ src/snowflake/connector/externals_utils/externals_setup.py
@@ -4,14 +4,22 @@ from logging import getLogger
 
 from ..secret_detector import SecretDetector
 
-URLLIB3_MODULE_NAME = "snowflake.connector.vendored.urllib3.connectionpool"
+URLLIB3_MODULE_NAME = "snowflake.connector.vendored.urllib3"
 # TODO: after migration to the external urllib3 from the vendored one, we should change filters here immediately to the below module's logger:
 # URLLIB3_MODULE_NAME = "urllib3"
 
 
-def add_formatters_to_loggers():
+def add_formatters_to_urllib3_loggers():
+    # Import to make sure the logger of urllib3 was set up already and handlers were added
+    import snowflake.connector.vendored.urllib3  # noqa: F401
+
     for handler in getLogger(URLLIB3_MODULE_NAME).handlers:
         handler.setFormatter(SecretDetector("%(asctime)s %(levelname)s %(message)s"))
 
 
-add_formatters_to_loggers()
+def setup_external_libraries():
+    """
+    Assures proper setup and injections before any external libraries are used.
+    """
+
+    add_formatters_to_urllib3_loggers()
