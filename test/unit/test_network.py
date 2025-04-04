5ed64c57131b578cbd4bb1f0534272e3b9d1d30e
Leonhard Spiegelberg <leonhard.spiegelberg@snowflake.com>
2025-04-04 11:37:24 -0700

    allow for UUID encoding

--- test/unit/test_network.py
+++ test/unit/test_network.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 import io
+import json
 import unittest.mock
+import uuid
+
+from src.snowflake.connector.network import SnowflakeRestfulJsonEncoder
 from test.unit.mock_utils import mock_connection
 
 import pytest
@@ -63,3 +67,14 @@ def test_fetch():
         # if no retry is set to False, the function raises an InterfaceError
         with pytest.raises(InterfaceError) as exc:
             assert rest.fetch(**default_parameters, no_retry=False)
+
+@pytest.mark.parametrize("u", [uuid.uuid1(),
+        uuid.uuid3(uuid.NAMESPACE_URL, "www.snowflake.com"),
+                             uuid.uuid4(),
+        uuid.uuid5(uuid.NAMESPACE_URL, "www.snowflake.com")])
+def test_json_serialize_uuid(u):
+    expected = f"{{\"u\": \"{u}\", \"a\": 42}}"
+
+    assert(json.dumps(u, cls=SnowflakeRestfulJsonEncoder)) == f"\"{u}\""
+
+    assert json.dumps({"u":u, "a":42}, cls=SnowflakeRestfulJsonEncoder) == expected
