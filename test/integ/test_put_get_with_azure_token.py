223fe6b8c0661a9286ddd326c63a37cb09b9182c
Filip Pawłowski <filip.pawlowski@snowflake.com>
2025-04-09 17:32:56 +0300

    Fpawlowski/snow 2011595 snowflake connector for python logging presigned ur ls (#2257)

--- test/integ/test_put_get_with_azure_token.py
+++ test/integ/test_put_get_with_azure_token.py
@@ -88,11 +88,13 @@ def test_put_get_with_azure(tmpdir, conn_cnx, from_path, caplog):
     for line in caplog.text.splitlines():
         if "blob.core.windows.net" in line and expected_token_prefix in line:
             azure_request_present = True
+            # getattr is used to stay compatible with old driver - before SECRET_STARRED_MASK_STR was added
             assert (
-                expected_token_prefix + SecretDetector.SECRET_STARRED_MASK_STR in line
+                expected_token_prefix
+                + getattr(SecretDetector, "SECRET_STARRED_MASK_STR", "****")
+                in line
             ), "connectionpool logger is leaking sensitive information"
 
-    # Connection pool is used on GitHub actions, but not always locally
     assert (
         azure_request_present
     ), "Azure URL was not found in logs, so it can't be assumed that no leaks happened in it"
