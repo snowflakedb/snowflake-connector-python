7a2b02ff1a3dd058d9e880ea61f81ce81b680bb4
Piotr Bulawa <piotr.bulawa@snowflake.com>
2025-04-09 11:11:05 +0200

    Fix expired S3 credentials update

--- src/snowflake/connector/s3_storage_client.py
+++ src/snowflake/connector/s3_storage_client.py
@@ -329,6 +329,9 @@ class SnowflakeS3RestClient(SnowflakeStorageClient):
             amzdate = t.strftime("%Y%m%dT%H%M%SZ")
             short_amzdate = amzdate[:8]
             x_amz_headers["x-amz-date"] = amzdate
+            x_amz_headers["x-amz-security-token"] = self.credentials.creds.get(
+                "AWS_TOKEN"
+            )
 
             (
                 canonical_request,
