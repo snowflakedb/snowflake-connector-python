607db2e1facc15389a17d43ceb39736aeee3e1d6
Mark Keller <mark.keller@snowflake.com>
2025-04-14 08:44:04 -0700

    SNOW-1825495 OAuth flows implementation (#2135)
    Co-authored-by: Michał Hofman <michal.hofman@snowflake.com>
    Co-authored-by: Piotr Bulawa <piotr.bulawa@snowflake.com>
    Co-authored-by: Maxim Mishchenko <maxim.mishchenko@snowflake.com>
    Co-authored-by: Mikołaj Kubik <mikolaj.kubik@snowflake.com>
    Co-authored-by: Yijun Xie <yijun.xie@snowflake.com>
    Co-authored-by: Zexin Yao <103003040+sfc-gh-zyao@users.noreply.github.com>
    Co-authored-by: Jakub Szczerbiński <jakub.szczerbinski@snowflake.com>
    Co-authored-by: Patryk Cyrek <patryk.cyrek@snowflake.com>

---
+++ ci/test_authentication.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -e
+
+set -o pipefail
+
+
+export THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+export WORKSPACE=${WORKSPACE:-/tmp}
+
+CI_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+if [[ -n "$JENKINS_HOME" ]]; then
+  ROOT_DIR="$(cd "${CI_DIR}/.." && pwd)"
+  export WORKSPACE=${WORKSPACE:-/tmp}
+  echo "Use /sbin/ip"
+  IP_ADDR=$(/sbin/ip -4 addr show scope global dev eth0 | grep inet | awk '{print $2}' | cut -d / -f 1)
+
+fi
+
+gpg --quiet --batch --yes --decrypt --passphrase="$PARAMETERS_SECRET" --output $THIS_DIR/../.github/workflows/parameters/private/parameters_aws_auth_tests.json "$THIS_DIR/../.github/workflows/parameters/private/parameters_aws_auth_tests.json.gpg"
+gpg --quiet --batch --yes --decrypt --passphrase="$PARAMETERS_SECRET" --output $THIS_DIR/../.github/workflows/parameters/private/rsa_keys/rsa_key.p8 "$THIS_DIR/../.github/workflows/parameters/private/rsa_keys/rsa_key.p8.gpg"
+gpg --quiet --batch --yes --decrypt --passphrase="$PARAMETERS_SECRET" --output $THIS_DIR/../.github/workflows/parameters/private/rsa_keys/rsa_key_invalid.p8 "$THIS_DIR/../.github/workflows/parameters/private/rsa_keys/rsa_key_invalid.p8.gpg"
+
+docker run \
+  -v $(cd $THIS_DIR/.. && pwd):/mnt/host \
+  -v $WORKSPACE:/mnt/workspace \
+  --rm \
+  nexus.int.snowflakecomputing.com:8086/docker/snowdrivers-test-external-browser-python:1 \
+  "/mnt/host/ci/container/test_authentication.sh"
