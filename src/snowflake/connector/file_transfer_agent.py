58acb9920bb55f8c12674400d8e5ae57f4dd5c92
Filip Pawłowski <filip.pawlowski@snowflake.com>
2025-05-14 13:44:56 +0200

    SNOW-2043523: Test original error

--- src/snowflake/connector/file_transfer_agent.py
+++ src/snowflake/connector/file_transfer_agent.py
@@ -884,11 +884,14 @@ class SnowflakeFileTransferAgent:
                 print("\n\n")
                 logger.error(f"_expand_filenames FILE: {file_name}")
                 logger.error(f"isabs: {os.path.isabs(file_name)}")
-                logger.error("\n\n")
 
                 if not os.path.isabs(file_name):
                     file_name = os.path.join(GET_CWD(), file_name)
-
+                    print(f"After extend: \n\n_expand_filenames FILE: {file_name}")
+                    print(os.path.isabs(file_name))
+                    print("\n\n")
+                    logger.error(f"After extend: _expand_filenames FILE: {file_name}")
+                    logger.error(f"isabs: {os.path.isabs(file_name)}")
                 if (
                     IS_WINDOWS
                     and len(file_name) > 2
