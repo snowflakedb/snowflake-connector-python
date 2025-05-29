5add122e58c1478a8cc64e1c003f961e996095a5
Maxim Mishchenko <maxim.mishchenko@snowflake.com>
2025-05-29 21:42:28 +0200

    SNOW-2114098 add a jenkins script to build/push a prober image to the cloud registries

---
+++ prober/Jenkinsfile.groovy
@@ -0,0 +1,62 @@
+import utilities.Credentials
+
+pipeline {
+    agent { label 'regular-memory-node' }
+
+    options {
+        ansiColor('xterm')
+        timestamps()
+    }
+
+    environment {
+        VAULT_CREDENTIALS = credentials('vault-jenkins')
+        COMMIT_SHA_SHORT = sh(script: 'git rev-parse --short HEAD', returnStdout: true).trim()
+        IMAGE_NAME = 'drivers/python-driver-prober'
+        TEAM_NAME = 'Snow Drivers'
+        TEAM_JIRA_DL = 'triage-snow-drivers-warsaw-dl'
+        TEAM_JIRA_AREA = 'Developer Platform'
+        TEAM_COMPONENT = 'Python Driver'
+    }
+
+    stages {
+        stage('Build Image') {
+            steps {
+                sh """
+                docker build \
+                -t ${IMAGE_NAME}:${COMMIT_SHA_SHORT} -f ./Dockerfile .
+                """
+            }
+        }
+
+        stage('Checkout Jenkins Push Scripts') {
+            steps {
+                dir('k8sc-jenkins_scripts') {
+                    git branch: 'master',
+                    credentialsId: Credentials.GithubAppCredentials.DEV_SNOWFLAKE_3.credentialId,
+                    url: 'https://github.com/snowflakedb/k8sc-jenkins_scripts.git'
+                }
+            }
+        }
+
+        stage('Push Image') {
+            steps {
+                sh """
+                ./k8sc-jenkins_scripts/push_image.sh \
+                -r ${VAULT_CREDENTIALS_USR} \
+                -s ${VAULT_CREDENTIALS_PSW} \
+                -i ${IMAGE_NAME} \
+                -v ${COMMIT_SHA_SHORT} \
+                -n ${TEAM_JIRA_DL} \
+                -a ${TEAM_JIRA_AREA} \
+                -C ${TEAM_JIRA_COMPONENT}
+                """
+            }
+        }
+
+        post {
+            always {
+                cleanWs()
+            }
+        }
+    }
+}
