254d6852ce5c84a239517a70167e0310f0abbcde
Maxim Mishchenko <maxim.mishchenko@snowflake.com>
2025-06-01 20:25:10 +0200

    SNOW-2114098 typo

--- prober/Jenkinsfile.groovy
+++ prober/Jenkinsfile.groovy
@@ -24,7 +24,7 @@ pipeline {
                     ls -l
                     docker build \
                     -t ${IMAGE_NAME}:${COMMIT_SHA_SHORT} \
-                    --label "org.opencontainers.image.revision=${COMMIT_SHA_SORT}" \
+                    --label "org.opencontainers.image.revision=${COMMIT_SHA_SHORT}" \
                     -f ./Dockerfile_min .
                     """
                 }
