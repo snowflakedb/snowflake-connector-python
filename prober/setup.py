53492f4ccc5a1de0bec70f18695d897ae1b17234
Patryk Cyrek <patryk.cyrek@snowflake.com>
2025-06-11 13:01:26 +0200

    SNOW-2114096-extending-probing-capabilities  (#2348)

--- prober/setup.py
+++ prober/setup.py
@@ -4,10 +4,7 @@ setup(
     name="snowflake_prober",
     version="1.0.0",
     packages=find_packages(),
-    install_requires=[
-        "snowflake-connector-python",
-        "requests",
-    ],
+    install_requires=["snowflake-connector-python", "requests", "faker"],
     entry_points={
         "console_scripts": [
             "prober=probes.main:main",
