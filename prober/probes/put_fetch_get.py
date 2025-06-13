b08df181f34f94975d851ef38dd46f95d2f8e6a1
Patryk Cyrek <patryk.cyrek@snowflake.com>
2025-06-13 16:04:31 +0200

    tmp folder

--- prober/probes/put_fetch_get.py
+++ prober/probes/put_fetch_get.py
@@ -397,7 +397,13 @@ def perform_put_fetch_get(connection_parameters: dict, num_records: int = 1000):
                 logger.error(f"Table {table_name} created")
 
                 logger.error("Generating random data")
-                file_name = generate_random_data(num_records, f"prober/probes/{table_name}.csv")
+
+                file_name = generate_random_data(num_records, f"prober/tmp/{table_name}.csv")
+
+                # Get the absolute path
+                absolute_path = os.path.abspath(file_name)
+                print(f"Absolute path: {absolute_path}") # DELETE
+
                 logger.error(f"Random data generated in {file_name}")
 
                 logger.error("PUT file to stage")
