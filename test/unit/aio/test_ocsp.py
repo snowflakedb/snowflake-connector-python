f7b3b9ab51ff47daf83777ba1ced5f5f2781d905
Patryk Czajka <patryk.czajka@snowflake.com>
2025-07-07 17:29:58 +0200

    Apply #2276 changes to async tests

--- test/unit/aio/test_ocsp.py
+++ test/unit/aio/test_ocsp.py
@@ -232,6 +232,7 @@ async def test_ocsp_bad_validity():
     del environ["SF_TEST_OCSP_FORCE_BAD_RESPONSE_VALIDITY"]
 
 
+@pytest.mark.flaky(reruns=3)
 async def test_ocsp_single_endpoint():
     environ["SF_OCSP_ACTIVATE_NEW_ENDPOINT"] = "True"
     SnowflakeOCSP.clear_cache()
@@ -255,6 +256,7 @@ async def test_ocsp_by_post_method():
             assert await ocsp.validate(url, connection), f"Failed to validate: {url}"
 
 
+@pytest.mark.flaky(reruns=3)
 async def test_ocsp_with_file_cache(tmpdir):
     """OCSP tests and the cache server and file."""
     tmp_dir = str(tmpdir.mkdir("ocsp_response_cache"))
@@ -268,6 +270,7 @@ async def test_ocsp_with_file_cache(tmpdir):
             assert await ocsp.validate(url, connection), f"Failed to validate: {url}"
 
 
+@pytest.mark.flaky(reruns=3)
 async def test_ocsp_with_bogus_cache_files(
     tmpdir, random_ocsp_response_validation_cache
 ):
@@ -308,6 +311,7 @@ async def test_ocsp_with_bogus_cache_files(
                 ), f"Failed to validate: {hostname}"
 
 
+@pytest.mark.flaky(reruns=3)
 async def test_ocsp_with_outdated_cache(tmpdir, random_ocsp_response_validation_cache):
     with mock.patch(
         "snowflake.connector.ocsp_snowflake.OCSP_RESPONSE_VALIDATION_CACHE",
@@ -367,6 +371,7 @@ async def _store_cache_in_file(tmpdir, target_hosts=None):
     return filename, target_hosts
 
 
+@pytest.mark.flaky(reruns=3)
 async def test_ocsp_with_invalid_cache_file():
     """OCSP tests with an invalid cache file."""
     SnowflakeOCSP.clear_cache()  # reset the memory cache
@@ -376,6 +381,7 @@ async def test_ocsp_with_invalid_cache_file():
             assert await ocsp.validate(url, connection), f"Failed to validate: {url}"
 
 
+@pytest.mark.flaky(reruns=3)
 @mock.patch(
     "snowflake.connector.aio._ocsp_snowflake.SnowflakeOCSP._fetch_ocsp_response",
     new_callable=mock.AsyncMock,
@@ -399,6 +405,7 @@ async def test_ocsp_cache_when_server_is_down(
         assert not cache_data, "no cache should present because of broken pipe"
 
 
+@pytest.mark.flaky(reruns=3)
 async def test_concurrent_ocsp_requests(tmpdir):
     """Run OCSP revocation checks in parallel. The memory and file caches are deleted randomly."""
     cache_file_name = path.join(str(tmpdir), "cache_file.txt")
