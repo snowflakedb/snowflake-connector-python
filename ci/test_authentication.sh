8332a1e729cc7ab2508f6d41c1e388ef9480b1a6
Patryk Cyrek <patryk.cyrek@snowflake.com>
2025-07-16 13:30:12 +0200

    SNOW-2205633: Migrating to new okta (#2407)
    (cherry picked from commit bcb8c80893345f4de5d136f3a23fce8039befede)

--- ci/test_authentication.sh
+++ ci/test_authentication.sh
@@ -23,5 +23,5 @@ docker run \
   -v $(cd $THIS_DIR/.. && pwd):/mnt/host \
   -v $WORKSPACE:/mnt/workspace \
   --rm \
-  nexus.int.snowflakecomputing.com:8086/docker/snowdrivers-test-external-browser-python:1 \
+    nexus.int.snowflakecomputing.com:8086/docker/snowdrivers-test-external-browser-python:2 \
   "/mnt/host/ci/container/test_authentication.sh"
