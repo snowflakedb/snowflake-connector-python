ffc824877321a297a7eae9f1aa6c42cb85d177b6
Filip Pawłowski <filip.pawlowski@snowflake.com>
2025-07-22 15:08:13 +0200

    add body to data

--- src/snowflake/connector/secret_detector.py
+++ src/snowflake/connector/secret_detector.py
@@ -43,7 +43,7 @@ class SecretDetector(logging.Formatter):
         r'"privateKeyData": "([a-z0-9/+=\\n]{10,})"', flags=re.MULTILINE | re.IGNORECASE
     )
     CONNECTION_TOKEN_PATTERN = re.compile(
-        r"(token|assertion content)" r"([\'\"\s:=]+)" r"([a-z0-9=/_\-\+\.]{8,})",
+        r"(token|assertion content)" r"([\'\"\s:= ]+)" r"([a-z0-9=/_\-\+\.]{8,})",
         flags=re.IGNORECASE,
     )
 
