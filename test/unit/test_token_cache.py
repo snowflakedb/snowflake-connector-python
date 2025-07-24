5a6a5a652c7469cb70fb588505a24a8c1c0c3421
Patryk Czajka <patryk.czajka@snowflake.com>
2025-07-24 16:01:28 +0200

    Add clear warning if the cache is not used

---
+++ test/unit/test_token_cache.py
@@ -0,0 +1,17 @@
+import pytest
+
+from snowflake.connector.token_cache import IS_LINUX, TokenCache
+
+
+@pytest.mark.skipif(not IS_LINUX, reason="File token cache is supported only on Linux")
+def test_file_token_cache_make_warns_incorrect_permissions_visible_on_stderr(
+    capsys, tmp_path, monkeypatch
+):
+    cache_dir = tmp_path / ".cache" / "snowflake"
+    cache_dir.mkdir(mode=0o740, parents=True)
+    monkeypatch.setenv("HOME", str(tmp_path))
+    TokenCache.make()
+    assert (
+        ".cache/snowflake has incorrect permissions. 740 != 0700'. Skipping it in cache directory lookup."
+        in capsys.readouterr().err
+    )
