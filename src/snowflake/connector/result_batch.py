954a890a06d8eea17bd37bf25fa4b42688ca952d
Filip Pawłowski <filip.pawlowski@snowflake.com>
2025-07-28 09:46:55 +0200

    Revert "SNOW-2203079: Make Session Manager pickleable"
    This reverts commit e74c40a952f9b0e89732a4815417fd1ccafa72b0.

--- src/snowflake/connector/result_batch.py
+++ src/snowflake/connector/result_batch.py
@@ -166,7 +166,7 @@ def create_batches_from_response(
                     column_converters,
                     cursor._use_dict_result,
                     json_result_force_utf8_decoding=cursor._connection._json_result_force_utf8_decoding,
-                    session_manager=cursor._connection.session_manager,
+                    connection=cursor._connection,
                 )
                 for c in chunks
             ]
@@ -181,7 +181,7 @@ def create_batches_from_response(
                     cursor._connection._numpy,
                     schema,
                     cursor._connection._arrow_number_to_decimal,
-                    session_manager=cursor._connection.session_manager,
+                    connection=cursor._connection,
                 )
                 for c in chunks
             ]
@@ -194,7 +194,7 @@ def create_batches_from_response(
             schema,
             column_converters,
             cursor._use_dict_result,
-            session_manager=cursor._connection.session_manager,
+            connection=cursor._connection,
         )
     elif rowset_b64 is not None:
         first_chunk = ArrowResultBatch.from_data(
@@ -205,7 +205,7 @@ def create_batches_from_response(
             cursor._connection._numpy,
             schema,
             cursor._connection._arrow_number_to_decimal,
-            session_manager=cursor._connection.session_manager,
+            connection=cursor._connection,
         )
     else:
         logger.error(f"Don't know how to construct ResultBatches from response: {data}")
@@ -217,7 +217,7 @@ def create_batches_from_response(
             cursor._connection._numpy,
             schema,
             cursor._connection._arrow_number_to_decimal,
-            session_manager=cursor._connection.session_manager,
+            connection=cursor._connection,
         )
 
     return [first_chunk] + rest_of_chunks
@@ -251,7 +251,7 @@ class ResultBatch(abc.ABC):
         remote_chunk_info: RemoteChunkInfo | None,
         schema: Sequence[ResultMetadataV2],
         use_dict_result: bool,
-        session_manager: SessionManager | None = None,
+        connection: SnowflakeConnection | None = None,
     ) -> None:
         self.rowcount = rowcount
         self._chunk_headers = chunk_headers
@@ -261,7 +261,7 @@ class ResultBatch(abc.ABC):
             [s._to_result_metadata_v1() for s in schema] if schema is not None else None
         )
         self._use_dict_result = use_dict_result
-        self._session_manager = session_manager
+        self._connection = connection
         self._metrics: dict[str, int] = {}
         self._data: str | list[tuple[Any, ...]] | None = None
         if self._remote_chunk_info:
@@ -316,6 +316,7 @@ class ResultBatch(abc.ABC):
     ) -> Response:
         """Downloads the data that the ``ResultBatch`` is pointing at."""
         sleep_timer = 1
+        connection = connection or self._connection
         backoff = (
             connection._backoff_generator
             if connection is not None
@@ -332,20 +333,15 @@ class ResultBatch(abc.ABC):
                         "timeout": DOWNLOAD_TIMEOUT,
                     }
                     # Try to reuse a connection if possible
-                    connection_session_manager = (
-                        connection.session_manager
-                        if (connection and connection.session_manager)
-                        else self._session_manager
-                    )
-                    if connection_session_manager is not None:
-                        with connection_session_manager.use_requests_session() as session:
+                    if connection and connection.session_manager is not None:
+                        with connection.session_manager.use_requests_session() as session:
                             logger.debug(
                                 f"downloading result batch id: {self.id} with existing session {session}"
                             )
                             response = session.request("get", **request_data)
                     else:
                         logger.debug(
-                            f"downloading result batch id: {self.id} with new session through local session manager"
+                            f"downloading result batch id: {self.id} with SessionManager fallback"
                         )
                         local_session_manager = SessionManager(use_pooling=False)
                         response = local_session_manager.get(**request_data)
@@ -448,7 +444,7 @@ class JSONResultBatch(ResultBatch):
         use_dict_result: bool,
         *,
         json_result_force_utf8_decoding: bool = False,
-        session_manager: SessionManager | None = None,
+        connection: SnowflakeConnection | None = None,
     ) -> None:
         super().__init__(
             rowcount,
@@ -456,7 +452,7 @@ class JSONResultBatch(ResultBatch):
             remote_chunk_info,
             schema,
             use_dict_result,
-            session_manager,
+            connection,
         )
         self._json_result_force_utf8_decoding = json_result_force_utf8_decoding
         self.column_converters = column_converters
@@ -469,7 +465,7 @@ class JSONResultBatch(ResultBatch):
         schema: Sequence[ResultMetadataV2],
         column_converters: Sequence[tuple[str, SnowflakeConverterType]],
         use_dict_result: bool,
-        session_manager: SessionManager | None = None,
+        connection: SnowflakeConnection | None = None,
     ):
         """Initializes a ``JSONResultBatch`` from static, local data."""
         new_chunk = cls(
@@ -479,7 +475,7 @@ class JSONResultBatch(ResultBatch):
             schema,
             column_converters,
             use_dict_result,
-            session_manager=session_manager,
+            connection=connection,
         )
         new_chunk._data = new_chunk._parse(data)
         return new_chunk
@@ -618,7 +614,7 @@ class ArrowResultBatch(ResultBatch):
         numpy: bool,
         schema: Sequence[ResultMetadataV2],
         number_to_decimal: bool,
-        session_manager: SessionManager | None = None,
+        connection: SnowflakeConnection | None = None,
     ) -> None:
         super().__init__(
             rowcount,
@@ -626,7 +622,7 @@ class ArrowResultBatch(ResultBatch):
             remote_chunk_info,
             schema,
             use_dict_result,
-            session_manager,
+            connection,
         )
         self._context = context
         self._numpy = numpy
@@ -689,7 +685,7 @@ class ArrowResultBatch(ResultBatch):
         numpy: bool,
         schema: Sequence[ResultMetadataV2],
         number_to_decimal: bool,
-        session_manager: SessionManager | None = None,
+        connection: SnowflakeConnection | None = None,
     ):
         """Initializes an ``ArrowResultBatch`` from static, local data."""
         new_chunk = cls(
@@ -701,7 +697,7 @@ class ArrowResultBatch(ResultBatch):
             numpy,
             schema,
             number_to_decimal,
-            session_manager=session_manager,
+            connection=connection,
         )
         new_chunk._data = data
 
