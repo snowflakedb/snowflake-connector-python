6d32c62c298a2e598cdb23ab93a258b97839ced2
Patryk Czajka <patryk.czajka@snowflake.com>
2025-08-07 15:39:14 +0200

    [ASYNC] Apply #2264 to async code

--- test/unit/aio/test_programmatic_access_token_async.py
+++ test/unit/aio/test_programmatic_access_token_async.py
@@ -27,7 +27,6 @@ def wiremock_client() -> Generator[WiremockClient | Any, Any, None]:
 
 
 @pytest.mark.skipolddriver
-@pytest.mark.asyncio
 async def test_valid_pat_async(wiremock_client: WiremockClient) -> None:
     wiremock_data_dir = (
         pathlib.Path(__file__).parent.parent.parent
@@ -65,7 +64,6 @@ async def test_valid_pat_async(wiremock_client: WiremockClient) -> None:
 
 
 @pytest.mark.skipolddriver
-@pytest.mark.asyncio
 async def test_invalid_pat_async(wiremock_client: WiremockClient) -> None:
     wiremock_data_dir = (
         pathlib.Path(__file__).parent.parent.parent
@@ -90,42 +88,3 @@ async def test_invalid_pat_async(wiremock_client: WiremockClient) -> None:
         await connection.connect()
 
     assert str(execinfo.value).endswith("Programmatic access token is invalid.")
-
-
-@pytest.mark.skipolddriver
-@pytest.mark.asyncio
-async def test_pat_as_password_async(wiremock_client: WiremockClient) -> None:
-    wiremock_data_dir = (
-        pathlib.Path(__file__).parent.parent.parent
-        / "data"
-        / "wiremock"
-        / "mappings"
-        / "auth"
-        / "pat"
-    )
-
-    wiremock_generic_data_dir = (
-        pathlib.Path(__file__).parent.parent.parent
-        / "data"
-        / "wiremock"
-        / "mappings"
-        / "generic"
-    )
-
-    wiremock_client.import_mapping(wiremock_data_dir / "successful_flow.json")
-    wiremock_client.add_mapping(
-        wiremock_generic_data_dir / "snowflake_disconnect_successful.json"
-    )
-
-    connection = SnowflakeConnection(
-        user="testUser",
-        authenticator=PROGRAMMATIC_ACCESS_TOKEN,
-        token=None,
-        password="some PAT",
-        account="testAccount",
-        protocol="http",
-        host=wiremock_client.wiremock_host,
-        port=wiremock_client.wiremock_http_port,
-    )
-    await connection.connect()
-    await connection.close()
