fee17df3e7642a0aad50403e71cd141a7e0cc74f
Filip Pawłowski <filip.pawlowski@snowflake.com>
2025-08-09 14:02:49 +0200

    [Async] Apply #2303 to async code
    # Conflicts:
    #	src/snowflake/connector/aio/_direct_file_operation_utils.py

--- test/unit/aio/test_bind_upload_agent_async.py
+++ test/unit/aio/test_bind_upload_agent_async.py
@@ -9,20 +9,22 @@ from unittest.mock import AsyncMock
 
 
 async def test_bind_upload_agent_uploading_multiple_files():
-    from snowflake.connector.aio._build_upload_agent import BindUploadAgent
+    from snowflake.connector.aio._bind_upload_agent import BindUploadAgent
 
     csr = AsyncMock(auto_spec=True)
     rows = [bytes(10)] * 10
     agent = BindUploadAgent(csr, rows, stream_buffer_size=10)
     await agent.upload()
-    assert csr.execute.call_count == 11  # 1 for stage creation + 10 files
+    assert csr.execute.call_count == 1  # 1 for stage creation
+    assert csr._upload_stream.call_count == 10  # 10 for 10 files
 
 
 async def test_bind_upload_agent_row_size_exceed_buffer_size():
-    from snowflake.connector.aio._build_upload_agent import BindUploadAgent
+    from snowflake.connector.aio._bind_upload_agent import BindUploadAgent
 
     csr = AsyncMock(auto_spec=True)
     rows = [bytes(15)] * 10
     agent = BindUploadAgent(csr, rows, stream_buffer_size=10)
     await agent.upload()
-    assert csr.execute.call_count == 11  # 1 for stage creation + 10 files
+    assert csr.execute.call_count == 1  # 1 for stage creation
+    assert csr._upload_stream.call_count == 10  # 10 for 10 files
