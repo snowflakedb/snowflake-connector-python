20df1677d6573e48e6831572e632b8cc43cc4452
Zexin Yao <zexin.yao@snowflake.com>
2025-08-19 16:23:59 -0700

    Zexin: add foraml build script for test pipeline

---
+++ ci/anaconda/run.sh
@@ -0,0 +1,46 @@
+
+# Before running, do something similar to the following in command line.
+# export WORKSPACE=/home/zyao/my_workspace;
+# export build_number=321;
+
+# Here miniconda-install.sh is just a installer that I downloaded from Anaconda official site,
+# https://repo.anaconda.com/miniconda/
+
+
+if [[ -z $WORKSPACE ]]; then
+  # Development on dev machine
+  WORKSPACE=$HOME
+fi
+
+DIRECTORY=`dirname $0`
+
+# ===== Build docker image =====
+cd $DIRECTORY
+
+cp ~/miniconda-install/miniconda-install.sh ./
+
+docker build --build-arg ARCH=$(uname -m) -t snowflake_connector_python_image -f Dockerfile .
+
+rm miniconda-install.sh || true
+# Go back to the original directory
+cd $DIRECTORY
+
+
+# Check to make sure repos exist to build conda packages
+if [[ -d $WORKSPACE/snowflake-connector-python ]]; then
+  echo "Check snowflake-connector-python repo exists - PASSED"
+else
+  echo "[FAILURE] Please clone snowflake-connector-python repo at $WORKSPACE/snowflake-connector-python"
+fi
+
+# Run packager in docker image
+docker run \
+  -v $WORKSPACE/snowflake-connector-python/:/repo/snowflake-connector-python \
+  -v $WORKSPACE/conda-bld:/repo/conda-bld \
+  -e SNOWFLAKE_CONNECTOR_PYTHON_BUILD_NUMBER=${build_number} \
+  snowflake_connector_python_image \
+  /repo/snowflake-connector-python/ci/anaconda/package_builder.sh
+
+# Cleanup image for disk space
+docker container prune -f
+docker rmi snowflake_connector_python_image
