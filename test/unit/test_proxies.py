03f72d1a0d03b8e031003c019a0bcfb3db5de8bf
Maxim Mishchenko <maxim.mishchenko@snowflake.com>
2025-08-22 19:07:57 +0200

    SNOW-2176524 adjust proxy support adapter to new requests

--- test/unit/test_proxies.py
+++ test/unit/test_proxies.py
@@ -40,6 +40,9 @@ def test_socks_5_proxy_missing_proxy_header_attribute(caplog):
         def connection_from_url(self, url):
             pass
 
+        def connection_from_host(self, host, *args, **kwargs):
+            pass
+
     def mock_proxy_manager_for_url_no_header(*args, **kwargs):
         return MockSOCKSProxyManager()
 
