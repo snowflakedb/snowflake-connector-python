297b18c96f0b8df1e14cae090d9cbb6fbc1673c5
Peter Mansour <peter.mansour@snowflake.com>
2025-08-29 00:37:40 -0700

    Update WIF integration tests to verify authenticated username + prepare for impersonation (#2510)

--- test/wif/test_wif.py
+++ test/wif/test_wif.py
@@ -22,6 +22,9 @@ Running tests locally:
 ACCOUNT = os.getenv("SNOWFLAKE_TEST_WIF_ACCOUNT")
 HOST = os.getenv("SNOWFLAKE_TEST_WIF_HOST")
 PROVIDER = os.getenv("SNOWFLAKE_TEST_WIF_PROVIDER")
+EXPECTED_USERNAME = os.getenv("SNOWFLAKE_TEST_WIF_USERNAME")
+IMPERSONATION_PATH = os.getenv("SNOWFLAKE_TEST_WIF_IMPERSONATION_PATH")
+EXPECTED_USERNAME_IMPERSONATION = os.getenv("SNOWFLAKE_TEST_WIF_USERNAME_IMPERSONATION")
 
 
 @pytest.mark.wif
@@ -33,8 +36,8 @@ def test_wif_defined_provider():
         "workload_identity_provider": PROVIDER,
     }
     assert connect_and_execute_simple_query(
-        connection_params
-    ), "Failed to connect with using WIF - automatic provider detection"
+        connection_params, EXPECTED_USERNAME
+    ), f"Failed to connect with using WIF using provider {PROVIDER}"
 
 
 @pytest.mark.wif
@@ -51,21 +54,48 @@ def test_should_authenticate_using_oidc():
     }
 
     assert connect_and_execute_simple_query(
-        connection_params
+        connection_params, expected_user=None
     ), "Failed to connect using WIF with OIDC provider"
+
+
+@pytest.mark.wif
+@pytest.mark.skip("Impersonation is still being developed")
+def test_should_authenticate_with_impersonation():
+    if not isinstance(IMPERSONATION_PATH, str) or not IMPERSONATION_PATH:
+        pytest.skip("Skipping test - IMPERSONATION_PATH is not set")
+
+    logger.debug(f"Using impersonation path: {IMPERSONATION_PATH}")
+    impersonation_path_list = IMPERSONATION_PATH.split(",")
+
+    connection_params = {
+        "host": HOST,
+        "account": ACCOUNT,
+        "authenticator": "WORKLOAD_IDENTITY",
+        "workload_identity_provider": PROVIDER,
+        "workload_identity_impersonation_path": impersonation_path_list,
+    }
+
+    assert connect_and_execute_simple_query(
+        connection_params, EXPECTED_USERNAME_IMPERSONATION
+    ), f"Failed to connect using WIF with provider {PROVIDER}"
 
 
 def is_provider_gcp() -> bool:
     return PROVIDER == "GCP"
 
 
-def connect_and_execute_simple_query(connection_params) -> bool:
+def connect_and_execute_simple_query(connection_params, expected_user=None) -> bool:
     try:
         logger.info("Trying to connect to Snowflake")
         with snowflake.connector.connect(**connection_params) as con:
-            result = con.cursor().execute("select 1;")
-            logger.debug(result.fetchall())
-            logger.info("Successfully connected to Snowflake")
+            result = con.cursor().execute("select current_user();")
+            (user,) = result.fetchone()
+            logger.debug(user)
+            if expected_user:
+                assert (
+                    expected_user == user
+                ), f"Expected user '{expected_user}', got user '{user}'"
+            logger.info(f"Successfully connected to Snowflake as {user}")
             return True
     except Exception as e:
         logger.error(e)
