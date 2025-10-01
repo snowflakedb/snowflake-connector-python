cf92a74609ada8df2586b44ab7943d5a5802c2cc
Mathias Florin <31008003+mathiasflorin@users.noreply.github.com>
2025-10-01 14:40:01 +0200

    Improve AWS region detection by checking AWS_DEFAULT_REGION as fallback (#2535)
    Co-authored-by: Patryk Czajka <patryk.czajka@snowflake.com>
    
    (cherry picked from commit ae2bf2e6f76bc92597153692752a6b7488961de9)

--- src/snowflake/connector/wif_util.py
+++ src/snowflake/connector/wif_util.py
@@ -96,10 +96,11 @@ def extract_iss_and_sub_without_signature_verification(jwt_str: str) -> tuple[st
 
 def get_aws_region() -> str:
     """Get the current AWS workload's region, or raises an error if it's missing."""
-    region = None
-    if "AWS_REGION" in os.environ:  # Lambda
-        region = os.environ["AWS_REGION"]
-    else:  # EC2
+
+    region = os.environ.get("AWS_REGION") or os.environ.get("AWS_DEFAULT_REGION")
+
+    if not region:
+        # Fallback for EC2 environments
         # TODO: SNOW-2223669 Investigate if our adapters - containing settings of http traffic - should be passed here as boto urllib3session. Those requests go to local servers, so they do not need Proxy setup or Headers customization in theory. But we may want to have all the traffic going through one class (e.g. Adapter or mixin).
         region = InstanceMetadataRegionFetcher().retrieve_region()
 
@@ -108,6 +109,7 @@ def get_aws_region() -> str:
             msg="No AWS region was found. Ensure the application is running on AWS.",
             errno=ER_WIF_CREDENTIALS_NOT_FOUND,
         )
+
     return region
 
 
