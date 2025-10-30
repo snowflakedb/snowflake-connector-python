7d91031c5bc6a7d4d6a4dd7717b4884a6b38cc56
Filip Pawłowski <filip.pawlowski@snowflake.com>
2025-10-30 17:40:42 +0100

    Maybe fix broken pipeline

--- setup.py
+++ setup.py
@@ -197,6 +197,8 @@ class SetDefaultInstallationExtras(egg_info):
 cmd_class["egg_info"] = SetDefaultInstallationExtras
 
 setup(
+    name="snowflake-connector-python",
+    description="Snowflake Connector for Python",
     version=version,
     ext_modules=extensions,
     cmdclass=cmd_class,
