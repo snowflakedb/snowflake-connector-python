7d5584ea70926d8bc047ee98599907bfbc6c55cc
Patryk Czajka <patryk.czajka@snowflake.com>
2025-10-31 13:00:19 +0100

    DEBUG async_test

--- ci/wif/test_wif.sh
+++ ci/wif/test_wif.sh
@@ -6,15 +6,16 @@ set -x
 export SF_OCSP_TEST_MODE=true
 export RUN_WIF_TESTS=true
 
+/opt/python/cp312-cp312/bin/python -m pip install --break-system-packages uv
 
 # setup pytest
-/opt/python/cp312-cp312/bin/python -m pip install --break-system-packages pytest pytest-asyncio
+/opt/python/cp312-cp312/bin/python -m uv pip install --break-system-packages pytest pytest-asyncio
 
-# test WIF without asyncio installed
-/opt/python/cp312-cp312/bin/python -m pip install --break-system-packages -e .
-/opt/python/cp312-cp312/bin/python -m pytest test/wif/ --ignore test/wif/test_wif_async.py
+# # test WIF without asyncio installed
+# /opt/python/cp312-cp312/bin/python -m uv pip install --break-system-packages -e .
+# /opt/python/cp312-cp312/bin/python -m pytest test/wif/ --ignore test/wif/test_wif_async.py
 
 # test WIF with asyncio installed
-/opt/python/cp312-cp312/bin/python -m pip install --break-system-packages -e '.[aio]'
+/opt/python/cp312-cp312/bin/python -m uv pip install --break-system-packages -e '.[aio]'
 # run all tests to see whether installation does not break anything
 /opt/python/cp312-cp312/bin/python -m pytest test/wif/
