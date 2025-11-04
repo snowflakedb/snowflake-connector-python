4ca6ed30ca8a371cf16c4f48534e606f8e070b3b
Zexin Yao <zexin.yao@snowflake.com>
2025-11-04 15:07:26 -0800

    update comment

--- ci/anaconda/package_builder.sh
+++ ci/anaconda/package_builder.sh
@@ -5,11 +5,9 @@ export CONDA_BLD_PATH=/repo/conda-bld
 
 mkdir -p $CONDA_BLD_PATH
 cd "$SNOWFLAKE_CONNECTOR_PYTHON_DIR"
+# Build with .tar.bz2 (pkg_format = 1) and .conda (pkg_format = 2).
 conda config --set conda_build.pkg_format 1
 bash ./ci/anaconda/conda_build.sh
-# NOTE: the below is to output the build in .conda format.
-# To do so, we set conda_build.pkg_format = 2 and then
-# remove it later to go back to default behavior.
 conda config --set conda_build.pkg_format 2
 bash ./ci/anaconda/conda_build.sh
 conda config --remove-key conda_build.pkg_format
