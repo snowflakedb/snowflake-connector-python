02c693a8708e3b2f167388bbbd8b6b4e58efe27b
Filip Pawłowski <filip.pawlowski@snowflake.com>
2025-11-04 20:14:31 +0100

    NO-SNOW: remove comment

--- src/snowflake/connector/aio/_connection.py
+++ src/snowflake/connector/aio/_connection.py
@@ -149,7 +149,6 @@ class SnowflakeConnection(SnowflakeConnectionSync):
         if "platform_detection_timeout_seconds" not in kwargs:
             self._platform_detection_timeout_seconds = 0.0
 
-        # TODO: why we have it here if never changed
         self._connected = False
         self.expired = False
         # check SNOW-1218851 for long term improvement plan to refactor ocsp code
