eab1cf6169240fbb7a569b955d119ff4c290a359
Tomasz Urbaszek <tomasz.urbaszek@snowflake.com>
2025-11-21 14:45:53 +0100

    fixup! SNOW-1902886: Validate account input

--- test/unit/test_parse_account.py
+++ test/unit/test_parse_account.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 
 import pytest
 
+from snowflake.connector import connect
 from snowflake.connector.util_text import is_valid_account_identifier, parse_account
 
 
@@ -17,17 +18,43 @@ def test_parse_account_basic():
 
 
 @pytest.mark.parametrize(
-    "value,expected",
+    "value",
     [
-        ("abc", True),
-        ("ABC", True),
-        ("a_b-c1", True),
-        ("a.b", False),
-        ("a/b", False),
-        ("a\\b", False),
-        ("", False),
-        ("snowflakecomputing.com", False),
+        "abc",
+        "aaa.bbb.ccc",
+        "aaa.bbb.ccc.ddd" "ABC",
+        "a_b-c1",
+        "account1",
+        "my_account",
+        "my-account",
+        "account_123",
+        "ACCOUNT_NAME",
     ],
 )
-def test_is_valid_account_identifier(value, expected):
-    assert is_valid_account_identifier(value) is expected
+def test_is_valid_account_identifier(value):
+    assert is_valid_account_identifier(value) is True
+
+
+@pytest.mark.parametrize(
+    "value",
+    [
+        "a/b",
+        "a\\b",
+        "",
+        "aa.bb.ccc/dddd",
+        "account@domain",
+        "account name",
+        "account\ttab",
+        "account\nnewline",
+        "account:port",
+        "account;semicolon",
+        "account'quote",
+        'account"doublequote',
+    ],
+)
+def test_is_invalid_account_identifier(value):
+    assert is_valid_account_identifier(value) is False
+    with pytest.raises(ValueError) as err:
+        connect(account=value, user="jdoe", password="***")
+
+    assert "Invalid account identifier" in str(err)
