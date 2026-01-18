e3abb54f9ad0cc485e7ce4d321128f13f29f1be5
myu65 <myu.laurant@gmail.com>
2026-01-18 13:27:01 +0000

    add glob escaoe

--- src/snowflake/connector/file_transfer_agent.py
+++ src/snowflake/connector/file_transfer_agent.py
@@ -879,6 +879,7 @@ class SnowflakeFileTransferAgent:
                     file_name = file_name[1:]
                 if not os.path.isabs(file_name):
                     file_name = os.path.abspath(file_name)
+                safe_file_name = glob.escape(file_name)
                 files = glob.glob(file_name)
                 canonical_locations += files
             else:
