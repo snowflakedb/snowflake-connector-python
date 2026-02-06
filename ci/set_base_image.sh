960b4ca2df8639f7199ca643cd9d16e13787ea24
Patryk Cyrek <patryk.cyrek@snowflake.com>
2026-02-06 14:19:27 +0100

    docker/

--- ci/set_base_image.sh
+++ ci/set_base_image.sh
@@ -8,9 +8,9 @@ set -o pipefail
 INTERNAL_REPO=artifactory.ci1.us-west-2.aws-dev.app.snowflake.com/internal-production-docker-snowflake-virtual
 if [[ -n "$JENKINS_HOME" ]]; then
     echo "[INFO] Pull docker images from $INTERNAL_REPO"
-    export BASE_IMAGE_MANYLINUX2014=$INTERNAL_REPO/manylinux2014_x86_64:2025.02.12-1
-    export BASE_IMAGE_MANYLINUX2014AARCH64=$INTERNAL_REPO/manylinux2014_aarch64:2025.02.12-1
-    export BASE_IMAGE_ROCKYLINUX9=$INTERNAL_REPO/rockylinux:9
+    export BASE_IMAGE_MANYLINUX2014=$INTERNAL_REPO/docker/manylinux2014_x86_64:2025.02.12-1
+    export BASE_IMAGE_MANYLINUX2014AARCH64=$INTERNAL_REPO/docker/manylinux2014_aarch64:2025.02.12-1
+    export BASE_IMAGE_ROCKYLINUX9=$INTERNAL_REPO/docker/rockylinux:9
 else
     echo "[INFO] Pull docker images from public registry"
     export BASE_IMAGE_MANYLINUX2014=quay.io/pypa/manylinux2014_x86_64
